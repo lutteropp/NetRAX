970da7263144ef60d80971594667993217e57906
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2019-09-04 13:14:37 +0200

    fixed network parsing, simple network test case works now

--- src/io/NetworkIO.cpp
+++ src/io/NetworkIO.cpp
@@ -10,6 +10,7 @@
 
 #include <array>
 #include <stdexcept>
+#include <iostream>
 
 namespace netrax {
 
@@ -19,7 +20,6 @@ namespace netrax {
 
 // TreeNode: [label][:branch_length] or [label] or [label][:branch_length][:support] or [] or [:branch_length] or [:branch_length][:support]
 // ReticulationNode: [label]#[type]i[:branch_length] or [label][:branch_length][:support][:inheritance_probability] or #[type]i[:branch_length] or #[type]i[:branch_length][:support] or #[type]i[:branch_length][:support][:inheritance_probability] or #[type]i
-
 
 Network convertNetwork(const unetwork_t& unetwork) {
 	Network network;
@@ -61,34 +61,52 @@ Network convertNetwork(const unetwork_t& unetwork) {
 			}
 		} else { // reticulation node
 			ReticulationData retData;
-			assert(unode->incoming); // link to first parent
-			assert(unode->next->incoming); // link to second parent
-			assert(!unode->next->next->incoming); // link to child
-			assert(unode->next->next->next == unode);
 
-			retData.init(unode->reticulation_index, unode->reticulation_name ? unode->reticulation_name : "", 0, &network.links[unode->node_index],
-					&network.links[unode->next->node_index], &network.links[unode->next->next->node_index], unode->prob);
-			network.nodes[unode->clv_index].initReticulation(unode->clv_index, unode->scaler_index, &network.links[unode->node_index],
-					unode->label, retData);
+			unetwork_node_t* first_parent_unode;
+			unetwork_node_t* second_parent_unode;
+			unetwork_node_t* child_unode;
+			if (unode->incoming) {
+				first_parent_unode = unode;
+				if (unode->next->incoming) {
+					second_parent_unode = unode->next;
+					child_unode = unode->next->next;
+				} else {
+					child_unode = unode->next;
+					second_parent_unode = unode->next->next;
+				}
+			} else {
+				child_unode = unode;
+				first_parent_unode = unode->next;
+				second_parent_unode = unode->next->next;
+			}
+			assert(first_parent_unode->incoming);
+			assert(second_parent_unode->incoming);
+			assert(!child_unode->incoming);
 
-			network.links[unode->node_index].init(unode->node_index, &network.nodes[unode->clv_index], &network.edges[unode->pmatrix_index],
-					&network.links[unode->next->node_index], &network.links[unode->back->node_index], Direction::INCOMING);
-			network.links[unode->next->node_index].init(unode->next->node_index, &network.nodes[unode->clv_index],
-					&network.edges[unode->next->pmatrix_index], &network.links[unode->next->next->node_index],
-					&network.links[unode->next->back->node_index], Direction::INCOMING);
-			network.links[unode->next->next->node_index].init(unode->next->next->node_index, &network.nodes[unode->clv_index],
-					&network.edges[unode->next->next->pmatrix_index], &network.links[unode->node_index],
-					&network.links[unode->next->next->back->node_index], Direction::OUTGOING);
+			retData.init(first_parent_unode->reticulation_index, first_parent_unode->reticulation_name ? first_parent_unode->reticulation_name : "", 0,
+					&network.links[first_parent_unode->node_index], &network.links[second_parent_unode->node_index],
+					&network.links[child_unode->node_index], first_parent_unode->prob);
+			network.nodes[first_parent_unode->clv_index].initReticulation(first_parent_unode->clv_index, first_parent_unode->scaler_index, &network.links[first_parent_unode->node_index],
+					first_parent_unode->label, retData);
 
-			network.edges[unode->pmatrix_index].init(unode->pmatrix_index, &network.links[unode->node_index],
-					&network.links[unode->back->node_index], unode->length);
-			network.edges[unode->next->pmatrix_index].init(unode->next->pmatrix_index, &network.links[unode->next->node_index],
-					&network.links[unode->next->back->node_index], unode->next->length);
-			network.edges[unode->next->next->pmatrix_index].init(unode->next->next->pmatrix_index,
-					&network.links[unode->next->next->node_index], &network.links[unode->next->next->back->node_index],
-					unode->next->next->length);
+			network.links[first_parent_unode->node_index].init(first_parent_unode->node_index, &network.nodes[first_parent_unode->clv_index], &network.edges[first_parent_unode->pmatrix_index],
+					&network.links[second_parent_unode->node_index], &network.links[first_parent_unode->back->node_index], Direction::INCOMING);
+			network.links[second_parent_unode->node_index].init(second_parent_unode->node_index, &network.nodes[second_parent_unode->clv_index],
+					&network.edges[second_parent_unode->pmatrix_index], &network.links[child_unode->node_index],
+					&network.links[second_parent_unode->back->node_index], Direction::INCOMING);
+			network.links[child_unode->node_index].init(child_unode->node_index, &network.nodes[child_unode->clv_index],
+					&network.edges[child_unode->pmatrix_index], &network.links[child_unode->node_index],
+					&network.links[child_unode->back->node_index], Direction::OUTGOING);
 
-			network.reticulation_nodes[unode->reticulation_index] = &network.nodes[unode->clv_index];
+			network.edges[first_parent_unode->pmatrix_index].init(first_parent_unode->pmatrix_index, &network.links[first_parent_unode->node_index],
+					&network.links[first_parent_unode->back->node_index], first_parent_unode->length);
+			network.edges[second_parent_unode->pmatrix_index].init(second_parent_unode->pmatrix_index, &network.links[second_parent_unode->node_index],
+					&network.links[second_parent_unode->back->node_index], second_parent_unode->length);
+			network.edges[child_unode->pmatrix_index].init(child_unode->pmatrix_index,
+					&network.links[child_unode->node_index], &network.links[child_unode->back->node_index],
+					child_unode->length);
+
+			network.reticulation_nodes[first_parent_unode->reticulation_index] = &network.nodes[first_parent_unode->clv_index];
 		}
 	}
 	network.root = &network.nodes[unetwork.vroot->clv_index];
