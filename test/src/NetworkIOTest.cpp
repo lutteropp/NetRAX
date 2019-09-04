e3657c2c73473e56d726bc532266992dd2336941
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2019-09-04 12:14:37 +0200

    added more test cases, reading simple network test case throws failed assertion

--- test/src/NetworkIOTest.cpp
+++ test/src/NetworkIOTest.cpp
@@ -23,3 +23,15 @@ TEST(NetworkIOTest, readRootedTree) {
 	Network network = readNetworkFromString(input);
 	ASSERT_EQ(4, network.tip_count);
 }
+
+TEST(NetworkIOTest, readUnrootedTree) {
+	std::string input = "(A:0.1,B:0.2,(C:0.3,D:0.4):0.5);";
+	Network network = readNetworkFromString(input);
+	ASSERT_EQ(4, network.tip_count);
+}
+
+TEST(NetworkIOTest, readSimpleNetwork) {
+	std::string input = "((A:2,((B:1,C:1)P:1)X#H1:0::0.3)Q:2,(D:2,X#H1:0::0.7)R:2);";
+	Network network = readNetworkFromString(input);
+	ASSERT_EQ(4, network.tip_count);
+}
