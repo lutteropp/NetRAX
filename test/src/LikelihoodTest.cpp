68377aba733a03598aa2b05a1c5d772709da6c7b
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2019-09-24 20:49:23 +0200

    added unit test for displayed tree of network to utree

--- test/src/LikelihoodTest.cpp
+++ test/src/LikelihoodTest.cpp
@@ -114,6 +114,14 @@ TEST (LikelihoodTest, displayedTreeOfTreeToUtree) {
 	ASSERT_NE(utree, nullptr);
 }
 
+TEST (LikelihoodTest, displayedTreeOfNetworkToUtree) {
+	std::string treePath = "examples/sample_networks/small.nw";
+	unetwork_t * unetwork = unetwork_parse_newick(treePath.c_str());
+	Network network = convertNetwork(*unetwork);
+	pll_utree_t * utree = displayed_tree_to_utree(network, 0);
+	ASSERT_NE(utree, nullptr);
+}
+
 TEST (LikelihoodTest, simpleTreeNoRepeatsNormalRaxml) {
 	std::string networkPath = "examples/sample_networks/tree.nw";
 	Network network = readNetworkFromFile(networkPath);
@@ -179,6 +187,33 @@ TEST (LikelihoodTest, simpleNetworkNoRepeatsOnlyDisplayedTreeWithRaxml) {
 	instance.opts.num_threads = 1;
 	instance.opts.use_repeats = false;
 	instance.opts.use_tip_inner = true;
+	load_parted_msa(instance);
+	check_options(instance);
+	balance_load(instance);
+	/* get partitions assigned to the current thread */
+	PartitionAssignment& part_assign = instance.proc_part_assign.at(ParallelContext::proc_id());
+
+	Tree tree(*(displayed_tree_to_utree(network, 0)));
+
+	TreeInfo raxml_treeinfo = TreeInfo(instance.opts, tree, *(instance.parted_msa.get()), instance.tip_msa_idmap, part_assign);
+	double network_logl = raxml_treeinfo.loglh(false);
+	std::cout << "The computed network_logl is: " << network_logl << "\n";
+	ASSERT_NE(network_logl, -std::numeric_limits<double>::infinity());
+}
+
+TEST (LikelihoodTest, simpleNetworkWithRepeatsOnlyDisplayedTreeWithRaxml) {
+	std::string networkPath = "examples/sample_networks/tree.nw";
+	Network network = readNetworkFromFile(networkPath);
+	std::string msaPath = "examples/sample_networks/small_fake_alignment.nw";
+
+	RaxmlInstance instance;
+	instance.opts = createDefaultOptions();
+	instance.opts.tree_file = networkPath;
+	instance.opts.msa_file = msaPath;
+	instance.opts.command = Command::evaluate;
+	instance.opts.num_threads = 1;
+	instance.opts.use_repeats = true;
+	instance.opts.use_tip_inner = false;
 	load_parted_msa(instance);
 	check_options(instance);
 	balance_load(instance);
