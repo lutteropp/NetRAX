12a10deb04e86a42103459ae4fd4571a1db0e959
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2019-09-27 23:18:25 +0200

    explicitly set the null members of the fake treeinfo

--- src/Fake.cpp
+++ src/Fake.cpp
@@ -92,6 +92,9 @@ int fake_init_tree(pllmod_treeinfo_t *treeinfo, Network &network) {
 	tree->edge_count = network.num_branches(); // +1 for the fake pmatrix index, TODO: Do we need it here?
 	tree->inner_count = network.num_inner();
 
+	tree->nodes = NULL;
+	tree->vroot = NULL;
+
 	treeinfo->root = NULL;
 
 	return PLL_SUCCESS;
@@ -253,14 +256,14 @@ TreeInfo create_fake_raxml_treeinfo(Network &network, const Options &opts,
 		const std::vector<uintVector> &site_weights) {
 	pllmod_treeinfo_t *base_treeinfo;
 
-	//base_treeinfo = create_fake_treeinfo(network, network.num_tips(), parted_msa.part_count(), opts.brlen_linkage);
+	base_treeinfo = create_fake_treeinfo(network, network.num_tips(), parted_msa.part_count(), opts.brlen_linkage);
 
-	std::cout << "just for debugging purposes, creating a normal treeinfo.\n";
+	/*std::cout << "just for debugging purposes, creating a normal treeinfo.\n";
 	pll_utree_t *displayed_tree = netrax::displayed_tree_to_utree(network, 0);
 
 	base_treeinfo = pllmod_treeinfo_create(displayed_tree->vroot,
 			displayed_tree->tip_count, parted_msa.part_count(),
-			opts.brlen_linkage);
+			opts.brlen_linkage);*/
 
 	return TreeInfo(opts, base_treeinfo, parted_msa, tip_msa_idmap, part_assign,
 			site_weights, fake_opt_brlen, fake_spr_round,
