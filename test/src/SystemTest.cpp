d103d5787a1cf71ee6717f7ea118fb273afb0769
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2019-10-30 13:16:55 +0100

    added unit system test for the tree case with normal raxml-ng, model optimization throws the same error there, too. Need to ask @alexey about that, maybe I did not understand the order in which raxml wants to have these optimizations executed

--- test/src/SystemTest.cpp
+++ test/src/SystemTest.cpp
@@ -25,6 +25,32 @@ TEST (SystemTest, testTheTest) {
 	ASSERT_TRUE(true);
 }
 
+TEST (SystemTest, allTreeOldRaxml) {
+	// initial setup
+	std::string treePath = "examples/sample_networks/tree.nw";
+	std::string msaPath = "examples/sample_networks/small_fake_alignment.nw";
+
+	Tree normalTree = Tree::loadFromFile(treePath);
+	NetraxOptions treeOptions;
+	treeOptions.network_file = treePath;
+	treeOptions.msa_file = msaPath;
+	treeOptions.use_repeats = true;
+	RaxmlWrapper treeWrapper = RaxmlWrapper(treeOptions);
+	TreeInfo info = treeWrapper.createRaxmlTreeinfo(normalTree.pll_utree_copy());
+
+	// initial logl computation
+	double initial_logl = info.loglh(false);
+	std::cout << "Initial loglikelihood: " << initial_logl << "\n";
+
+	// model parameter optimization
+	double modelopt_logl = info.optimize_model(treeWrapper.getRaxmlOptions().lh_epsilon);
+	std::cout << "Loglikelihood after model optimization: " << modelopt_logl << "\n";
+
+	// branch length optimization
+	double brlenopt_logl = info.optimize_branches(treeWrapper.getRaxmlOptions().lh_epsilon, 1);
+	std::cout << "Loglikelihood after branch length optimization: " << brlenopt_logl << "\n";
+}
+
 TEST (SystemTest, allTree) {
 	// initial setup
 	std::string treePath = "examples/sample_networks/tree.nw";
