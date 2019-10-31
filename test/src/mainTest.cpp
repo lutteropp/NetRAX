b1bb38c9843e1725ddab09df367dc313ac6941e0
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2019-10-31 17:32:14 +0100

    rmeoved some olddebug output, re-enabled all tests

--- test/src/mainTest.cpp
+++ test/src/mainTest.cpp
@@ -22,7 +22,7 @@ int main(int argc, char** argv)
   ::testing::AddGlobalTestEnvironment(env);
   //testing::GTEST_FLAG(filter) = "-NetworkIOTest.*";
   //::testing::GTEST_FLAG(filter) = "*SystemTest.allTree";
-  ::testing::GTEST_FLAG(filter) = "*BrlenOptTest.*";
+  //::testing::GTEST_FLAG(filter) = "*BrlenOptTest.*";
 
   auto result = RUN_ALL_TESTS();
 //  MPI_FINALIZE();
