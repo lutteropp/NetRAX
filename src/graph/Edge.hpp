bd8ced09c506eb829c568949b81908124d818035
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2019-11-28 14:25:51 +0100

    fixed missing data member initialization

--- src/graph/Edge.hpp
+++ src/graph/Edge.hpp
@@ -14,7 +14,7 @@ namespace netrax {
 struct Link;
 struct Edge {
 	Edge() :
-			pmatrix_index(0), link1(nullptr), link2(nullptr), length(0.0) {
+			pmatrix_index(0), link1(nullptr), link2(nullptr), length(0.0), support(0.0) {
 	}
 
 	void init(size_t index, Link* link1, Link* link2, double length) {
