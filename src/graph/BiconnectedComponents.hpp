496b003563c0fdd5ac3b0de00633ec9bba2ce2c9
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2020-01-17 20:21:42 +0100

    removed unneccessary blob tree data structure

--- src/graph/BiconnectedComponents.hpp
+++ src/graph/BiconnectedComponents.hpp
@@ -17,20 +17,4 @@ namespace netrax {
 		std::vector<unsigned int> blob_size;
 	};
 	BlobInformation partitionNetworkEdgesIntoBlobs(const Network& network);
-
-	struct BlobNode;
-	struct BlobEdge {
-		double length;
-		std::unique_ptr<BlobNode> child;
-	};
-
-	struct BlobNode {
-		unsigned int id;
-		Network blobNetwork;
-		std::vector<BlobEdge> children;
-	};
-
-	struct BlobTree {
-		std::unique_ptr<BlobNode> root;
-	};
 }
