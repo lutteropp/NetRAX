0d9074e02c4c7827dfdd037f8cea868c9727efcb
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2020-01-17 17:22:12 +0100

    Started writing simple BlobTree data structure

--- src/graph/BiconnectedComponents.hpp
+++ src/graph/BiconnectedComponents.hpp
@@ -8,6 +8,7 @@
 #include "Network.hpp"
 
 #include <vector>
+#include <memory>
 
 namespace netrax {
 	// A blob is a biconnected component in the underlying undirected graph of the phylogenetic network.
@@ -16,4 +17,20 @@ namespace netrax {
 		std::vector<unsigned int> blob_size;
 	};
 	BlobInformation partitionNetworkEdgesIntoBlobs(const Network& network);
+
+	struct BlobNode;
+	struct BlobEdge {
+		double length;
+		std::unique_ptr<BlobNode> child;
+	};
+
+	struct BlobNode {
+		unsigned int id;
+		Network blobNetwork;
+		std::vector<BlobEdge> children;
+	};
+
+	struct BlobTree {
+		std::unique_ptr<BlobNode> root;
+	};
 }
