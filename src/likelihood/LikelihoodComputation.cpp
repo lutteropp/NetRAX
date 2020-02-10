df6751d26512986c9b8256a7fd4e076957f89eef
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2020-02-10 21:45:34 +0100

    some rogue debug code falsified the likelhoods; was easy to find thanks to using git. Both blob and noblob likelihood computations now give the correct result for the small network

--- src/likelihood/LikelihoodComputation.cpp
+++ src/likelihood/LikelihoodComputation.cpp
@@ -187,17 +187,6 @@ double compute_tree_logl(Network &network, pllmod_treeinfo_t &fake_treeinfo, siz
 	std::cout << "tree_partition_logl for subnetwork root clv index " << ops_root->clv_index << ", tree_idx "
 			<< tree_idx << ": " << tree_partition_logl << "\n";
 
-	// just for debug: compute tree partition logl for all nodes:
-	for (size_t i = 0; i < network.nodes.size(); ++i) {
-		Node *node = &network.nodes[i];
-		Node *nodeBack = node->getLink()->getTargetNode();
-		double l = pll_compute_edge_loglikelihood(fake_treeinfo.partitions[partition_idx], node->getClvIndex(),
-				node->getScalerIndex(), nodeBack->getClvIndex(), nodeBack->getScalerIndex(),
-				node->getLink()->edge->pmatrix_index, fake_treeinfo.param_indices[partition_idx],
-				persite_logl->empty() ? nullptr : persite_logl->data());
-		std::cout << "tree_partition_logl for subnetwork root clv index " << node->clv_index << ", tree_idx "
-				<< tree_idx << ": " << l << "\n";
-	}
 	// just for debug 2: print all clv vectors
 	for (size_t i = 0; i < network.nodes.size(); ++i) {
 		print_clv_vector(fake_treeinfo, tree_idx, partition_idx, i);
@@ -242,18 +231,6 @@ void compute_tree_logl_blobs(Network &network, BlobInformation &blobInfo, const 
 				fake_treeinfo.param_indices[partition_idx], persite_logl->empty() ? nullptr : persite_logl->data());
 		std::cout << "tree_partition_logl for megablob root clv index " << ops_root->clv_index << ", tree_idx "
 				<< tree_idx << ": " << tree_partition_logl << "\n";
-	}
-
-	// just for debug: compute tree partition logl for all nodes:
-	for (size_t i = 0; i < network.nodes.size(); ++i) {
-		Node *node = &network.nodes[i];
-		Node *nodeBack = node->getLink()->getTargetNode();
-		double l = pll_compute_edge_loglikelihood(fake_treeinfo.partitions[partition_idx], node->getClvIndex(),
-				node->getScalerIndex(), nodeBack->getClvIndex(), nodeBack->getScalerIndex(),
-				node->getLink()->edge->pmatrix_index, fake_treeinfo.param_indices[partition_idx],
-				persite_logl->empty() ? nullptr : persite_logl->data());
-		std::cout << "tree_partition_logl for subnetwork root clv index " << node->clv_index << ", tree_idx "
-				<< tree_idx << ": " << l << "\n";
 	}
 
 	// just for debug 2: print all clv vectors
