085c41e12c3419e57a51cf855f5590b1746e2d8f
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2020-02-11 01:25:07 +0100

    Added chained 3 reticulations test case

--- test/src/mainTest.cpp
+++ test/src/mainTest.cpp
@@ -24,7 +24,8 @@ int main(int argc, char** argv)
   //::testing::GTEST_FLAG(filter) = "*SystemTest.allTree";
   //::testing::GTEST_FLAG(filter) = "*LikelihoodTest.celineNetwork:*LikelihoodTest.likelihoodFunctions*";
   //::testing::GTEST_FLAG(filter) = "*NetworkIOTest.reticulationHasLeafChild"*;
-  ::testing::GTEST_FLAG(filter) = "*LikelihoodTest.likelihoodFunctionsNetworkTiny:*LikelihoodTest.likelihoodFunctionsNetwork2Reticulations";
+  //::testing::GTEST_FLAG(filter) = "*LikelihoodTest.likelihoodFunctionsNetwork3Reticulations";
+  ::testing::GTEST_FLAG(filter) = "*LikelihoodTest.*";
 
   auto result = RUN_ALL_TESTS();
 //  MPI_FINALIZE();
