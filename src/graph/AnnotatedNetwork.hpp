a1a3bd576ce9fbddd28555c55dec38e24975cbb6
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2020-02-25 15:05:54 +0100

    created annotated network struct

---
+++ src/graph/AnnotatedNetwork.hpp
@@ -0,0 +1,23 @@
+/*
+ * AnnotatedNetwork.hpp
+ *
+ *  Created on: Feb 25, 2020
+ *      Author: Sarah Lutteropp
+ */
+
+#pragma once
+
+#include "Network.hpp"
+#include "NetworkFunctions.hpp"
+#include "BiconnectedComponents.hpp"
+
+namespace netrax {
+
+struct AnnotatedNetwork {
+	Network network; // The network topology itself
+	std::vector<Node*> parent; // Pointers to the parent nodes of all network nodes
+	BlobInformation blobInfo; // mapping of edges to blobs, megablob roots, mapping of megablob roots to set of reticulation nodes within the megablob
+	std::vector<Node*> topoTrav; // traversal in topological order
+};
+
+}
