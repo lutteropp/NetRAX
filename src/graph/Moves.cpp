f8b98d74f1ff7092f89b4bcb81bfc09c6606dd38
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2020-04-16 17:36:40 +0200

    added function that gathers all possible choices for s,t in the rNNI moves

--- src/graph/Moves.cpp
+++ src/graph/Moves.cpp
@@ -30,17 +30,49 @@ namespace netrax {
         return false;
     }
 
+	/*
+	 * we need to choose s and t in a way that there are connections {u,s} and {v,t},
+	 * but there are no connections {u,t} and {v,s}
+	*/
+    std::vector<std::pair<Node*, Node*> > getSTChoices(const Edge& edge) {
+    	std::vector<std::pair<Node*, Node*> > res;
+    	Node* u = edge.getSource();
+    	Node* v = edge.getTarget();
+
+    	auto uNeighbors = u->getNeighbors();
+    	auto vNeighbors = v->getNeighbors();
+
+    	for (const auto& s : uNeighbors) {
+    		if (s == v) continue;
+    		for (const auto& t : vNeighbors) {
+    			if (t == u) continue;
+
+    			if (std::find(uNeighbors.begin(), uNeighbors.end(), t) == uNeighbors.end()
+    					&& std::find(vNeighbors.begin(), vNeighbors.end(), s) == vNeighbors.end()) {
+    				res.emplace_back(std::make_pair(s, t));
+    			}
+    		}
+    	}
+    	return res;
+    }
+
     std::vector<RNNIMove> possibleRNNIMoves(const Network& network, const Edge& edge) {
     	std::vector<RNNIMove> res;
+
+
+
     	throw std::runtime_error("Not implemented yet");
     	return res;
     }
 
-    void performRNNIMove(Network& network, RNNIMove& move) {
+    void performMove(Network& network, RNNIMove& move, Edge& edge) {
+    	Node* u = edge.getSource();
+    	Node* v = edge.getTarget();
+
     	throw std::runtime_error("Not implemented yet");
     }
 
-    void undoRNNIMove(Network&network, RNNIMove& move, Edge& edge) {
+    void undoMove(Network&network, RNNIMove& move, Edge& edge) {
     	throw std::runtime_error("Not implemented yet");
     }
 }
--- src/graph/Moves.hpp
+++ src/graph/Moves.hpp
@@ -12,11 +12,16 @@
 namespace netrax {
 
 	enum class RNNIMove {
-		A1, A2, B1, B2, C1, C2, D
+		A1, A2, A1_alt, A2_alt, B1, B2, C1, C2, D, D_alt
+		/*
+		 * The moves correspond to the moves in this paper: https://doi.org/10.1371/journal.pcbi.1005611
+		 * A1 is for 1, A2 is for 1*, B1 is for 2, B2 is for 2*, C1 is for 3, C2 is for 3*, D is for 4.
+		 * The alt moves are for handling that the root node has 3 children in our network data structure.
+		 */
 	};
 
 	std::vector<RNNIMove> possibleRNNIMoves(const Network& network, const Edge& edge);
-	void performRNNIMove(Network& network, RNNIMove& move);
-	void undoRNNIMove(Network&network, RNNIMove& move, Edge& edge);
+	void performMove(Network& network, RNNIMove& move, Edge& edge);
+	void undoMove(Network&network, RNNIMove& move, Edge& edge);
 
 }
