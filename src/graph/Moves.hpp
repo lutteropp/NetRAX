ed6231af16757d5ce586d15f388c2bcfe7476e9f
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2020-04-16 17:00:05 +0200

    added API functions for rNNI moves

--- src/graph/Moves.hpp
+++ src/graph/Moves.hpp
@@ -10,5 +10,13 @@
 #include "Common.hpp"
 
 namespace netrax {
+
+	enum class RNNIMove {
+		A1, A2, B1, B2, C1, C2, D
+	};
+
+	std::vector<RNNIMove> possibleRNNIMoves(const Network& network, const Edge& edge);
+	void performRNNIMove(Network& network, RNNIMove& move);
+
     bool hasPath(const Network& network, const Node* from, const Node* to);
 }
