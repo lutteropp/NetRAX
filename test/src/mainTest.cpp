087eb646c114823a4dd594f3ea55bac73391b6a6
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2020-04-21 23:32:57 +0200

    switched to enabling the unit tests for the moves, currently getting failed assertion there

--- test/src/mainTest.cpp
+++ test/src/mainTest.cpp
@@ -24,7 +24,7 @@ int main(int argc, char **argv) {
     //::testing::GTEST_FLAG(filter) = "*LikelihoodTest.celineNetwork:*LikelihoodTest.likelihoodFunctions*";
     //::testing::GTEST_FLAG(filter) = "*NetworkIOTest.reticulationHasLeafChild"*;
     //::testing::GTEST_FLAG(filter) = "*LikelihoodTest.smallNetworkWithRepeats";
-    ::testing::GTEST_FLAG(filter) = "*LikelihoodTest.*";
+    ::testing::GTEST_FLAG(filter) = "*MovesTest.*";
 
     auto result = RUN_ALL_TESTS();
 //  MPI_FINALIZE();
