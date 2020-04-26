b6ff3bdf68fcbf89e77032968bff59f67d2630bc
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2020-04-27 01:13:08 +0200

    fixed removeInnerNode and removeEdge again, unit test still fails with same failed assertion

--- src/graph/Moves.cpp
+++ src/graph/Moves.cpp
@@ -739,7 +739,7 @@ void removeEdge(Network &network, Edge *edge) {
     size_t other_index = network.edges[network.branchCount - 1].pmatrix_index;
     size_t index_in_edges_array = (network.edges_by_index[index] - &network.edges[0]) / sizeof(Edge*);
     std::swap(network.edges[index_in_edges_array], network.edges[network.branchCount - 1]);
-    network.edges_by_index[other_index] = &network.edges[index];
+    network.edges_by_index[other_index] = &network.edges[index_in_edges_array];
     network.edges_by_index[index] = nullptr;
     network.nodeCount--;
 }
@@ -774,7 +774,7 @@ void removeNode(Network &network, Node *node) {
     size_t other_index = network.nodes[network.nodeCount - 1].clv_index;
     size_t index_in_nodes_array = (network.nodes_by_index[index] - &network.nodes[0]) / sizeof(Node*);
     std::swap(network.nodes[index_in_nodes_array], network.nodes[network.nodeCount - 1]);
-    network.nodes_by_index[other_index] = &network.nodes[index];
+    network.nodes_by_index[other_index] = &network.nodes[index_in_nodes_array];
     network.nodes_by_index[index] = nullptr;
     network.nodeCount--;
     if (node->type == NodeType::RETICULATION_NODE) {
