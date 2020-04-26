c4ed06c415a3cf643feeec29b413adcd24a44731
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2020-04-27 01:17:08 +0200

    found the bug, index in nodes/edge array is wrongly computed in removeNode/removeEdge... added an assertion to check it

--- src/graph/Moves.cpp
+++ src/graph/Moves.cpp
@@ -738,6 +738,7 @@ void removeEdge(Network &network, Edge *edge) {
     size_t index = edge->pmatrix_index;
     size_t other_index = network.edges[network.branchCount - 1].pmatrix_index;
     size_t index_in_edges_array = (network.edges_by_index[index] - &network.edges[0]) / sizeof(Edge*);
+    assert(network.edges[index_in_edges_array].pmatrix_index == index);
     std::swap(network.edges[index_in_edges_array], network.edges[network.branchCount - 1]);
     network.edges_by_index[other_index] = &network.edges[index_in_edges_array];
     network.edges_by_index[index] = nullptr;
@@ -773,6 +774,7 @@ void removeNode(Network &network, Node *node) {
     size_t index = node->clv_index;
     size_t other_index = network.nodes[network.nodeCount - 1].clv_index;
     size_t index_in_nodes_array = (network.nodes_by_index[index] - &network.nodes[0]) / sizeof(Node*);
+    assert(network.nodes[index_in_nodes_array].clv_index == index);
     std::swap(network.nodes[index_in_nodes_array], network.nodes[network.nodeCount - 1]);
     network.nodes_by_index[other_index] = &network.nodes[index_in_nodes_array];
     network.nodes_by_index[index] = nullptr;
