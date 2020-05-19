d01161a7b05cdbc77cc50ff22ba4a3d8c5101654
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2020-05-19 23:29:17 +0200

    removed the reticulation probs from the number of free parameters in the aic/bic computations

--- src/optimization/TopologyOptimization.cpp
+++ src/optimization/TopologyOptimization.cpp
@@ -24,8 +24,7 @@ double aic(AnnotatedNetwork &ann_network, double logl) {
     Network &network = ann_network.network;
     bool unlinked_mode = (ann_network.fake_treeinfo->brlen_linkage == PLLMOD_COMMON_BRLEN_UNLINKED);
     size_t multiplier = (unlinked_mode) ? 1 : ann_network.fake_treeinfo->partition_count;
-    size_t param_count = multiplier * (network.num_branches() + network.num_reticulations())
-            + ann_network.total_num_model_parameters;
+    size_t param_count = multiplier * network.num_branches() + ann_network.total_num_model_parameters;
     return aic(logl, param_count);
 }
 
@@ -33,8 +32,7 @@ double bic(AnnotatedNetwork &ann_network, double logl) {
     Network &network = ann_network.network;
     bool unlinked_mode = (ann_network.fake_treeinfo->brlen_linkage == PLLMOD_COMMON_BRLEN_UNLINKED);
     size_t multiplier = (unlinked_mode) ? 1 : ann_network.fake_treeinfo->partition_count;
-    size_t param_count = multiplier * (network.num_branches() + network.num_reticulations())
-            + ann_network.total_num_model_parameters;
+    size_t param_count = multiplier * network.num_branches() + ann_network.total_num_model_parameters;
     size_t num_sites = ann_network.total_num_sites;
     return bic(logl, param_count, num_sites);
 }
