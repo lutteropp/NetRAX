d6325f56e6ec3c6dafc27f4a7ad5f513a4e847b6
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2020-05-23 03:13:01 +0200

    using same order in main function as in random network system test

--- src/main.cpp
+++ src/main.cpp
@@ -26,17 +26,16 @@ int main(int argc, char **argv) {
 
     netrax::AnnotatedNetwork ann_network = netrax::build_annotated_network(netraxOptions);
     std::cout << "The current Likelihood model being used is the DNA model from raxml-ng\n\n";
-    std::cout << "Initial network loglikelihood: " << netrax::computeLoglikelihood(ann_network) << "\n";
-    std::cout << "After updating reticulation probs: " << netrax::updateReticulationProbs(ann_network) << "\n";
-    std::cout << "After doing model optimization: " << netrax::optimizeModel(ann_network) << "\n";
-    std::cout << "After updating reticulation probs again: " << netrax::updateReticulationProbs(ann_network) << "\n";
-    std::cout << "After branch length optimization (by averaging over per-tree optimized brlens in easy cases): "
-            << netrax::optimizeBranches(ann_network) << "\n";
-    std::cout << "After updating reticulation probs again again: " << netrax::updateReticulationProbs(ann_network)
-            << "\n";
-    std::cout << "After updating topology: " << netrax::optimizeTopology(ann_network) << "\n";
-    std::cout << "After updating reticulation probs again again again: " << netrax::updateReticulationProbs(ann_network)
-            << "\n";
+
+    computeLoglikelihood(ann_network);
+    optimizeBranches(ann_network);
+    updateReticulationProbs(ann_network);
+    optimizeModel(ann_network);
+    optimizeTopology(ann_network);
+    optimizeBranches(ann_network);
+    updateReticulationProbs(ann_network);
+    optimizeModel(ann_network);
+
     if (!netraxOptions.output_file.empty()) {
         netrax::writeNetwork(ann_network, netraxOptions.output_file);
         std::cout << "Final network written to " << netraxOptions.output_file << "\n";
