2ae0ae10691721f59d1ee6ce9a326c6374e59db3
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2020-06-03 00:19:52 +0200

    found another now failing unit test, it complains about brlen optimization suddenly getting a worse logl... so maybe I broke some logl computation stuff in one of the previous bugfixes?

--- test/src/mainTest.cpp
+++ test/src/mainTest.cpp
@@ -31,8 +31,8 @@ int main(int argc, char **argv) {
     //::testing::GTEST_FLAG(filter) = "*MovesTest.incrementalLoglikelihoodProblem*";
     //::testing::GTEST_FLAG(filter) = "*SystemTest.allNetwork*";
     //::testing::GTEST_FLAG(filter) = "*SystemTest.random*";
-    ::testing::GTEST_FLAG(filter) = "*SystemTest.problem15";
-    //::testing::GTEST_FLAG(filter) = "*SystemTest.problem15:*SystemTest.problem16";
+    ::testing::GTEST_FLAG(filter) = "*SystemTest.problem6*";
+    //::testing::GTEST_FLAG(filter) = "*SystemTest.problem*";
 
     auto result = RUN_ALL_TESTS();
 //  MPI_FINALIZE();
