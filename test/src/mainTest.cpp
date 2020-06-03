dc9cd7c39217e62f202ccec98f66bca70be2d1c3
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2020-06-03 16:02:37 +0200

    switched back to random network system test, all known bugs are fixed now! :)

--- test/src/mainTest.cpp
+++ test/src/mainTest.cpp
@@ -30,9 +30,8 @@ int main(int argc, char **argv) {
     //::testing::GTEST_FLAG(filter) = "*MovesTest.*";
     //::testing::GTEST_FLAG(filter) = "*MovesTest.incrementalLoglikelihoodProblem*";
     //::testing::GTEST_FLAG(filter) = "*SystemTest.allNetwork*";
-    //::testing::GTEST_FLAG(filter) = "*SystemTest.random*";
+    ::testing::GTEST_FLAG(filter) = "*SystemTest.random*";
     //::testing::GTEST_FLAG(filter) = "*SystemTest.problem*";
-    ::testing::GTEST_FLAG(filter) = "*SystemTest.problem21";
 
     auto result = RUN_ALL_TESTS();
 //  MPI_FINALIZE();
