b8ab817ddb3682f94d0fb5bdaebf2bc89e69df00
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2020-10-01 16:37:24 +0200

    Added calling network inference with NetRAX

--- experiments/netrax_wrapper.py
+++ experiments/netrax_wrapper.py
@@ -4,7 +4,7 @@ import os
 NETRAX_PATH = "/home/sarah/code-workspace/NetRAX/bin/netrax"
 
 
-# Uses netrax to compute the number of reticulations, BIC score, and loglikelihood of a network for a given MSA
+# Uses NetRAX to compute the number of reticulations, BIC score, and loglikelihood of a network for a given MSA
 def score_network(network_path, msa_path):
     netrax_cmd = NETRAX_PATH + " --score_only" + " --start_network" + network_path + " --msa " + msa_path
     netrax_output = subprocess.getoutput(netrax_cmd).splitlines()
@@ -17,3 +17,11 @@ def score_network(network_path, msa_path):
        if line.startswith("Loglikelihood:"):
             logl = float(line.split(": ")[1])
     return n_reticulations, bic, logl
+    
+    
+# Uses NetRAX to infer a network... uses single random starting network if timeout==0, else keeps searching for a better network until timeout seconds have passed.
+def infer_network(msa_path, output_path, timeout):
+    netrax_cmd = NETRAX_PATH + " --msa " + msa_path + " --output " + output_path
+    if timeout > 0:
+        netrax_cmd += " --endless --timeout " + str(timeout)
+    subprocess.getoutput(netrax_cmd).splitlines()
