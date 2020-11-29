82c3ca689f1b34ddb3b70e2f51212a9ee264bc42
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2020-11-29 23:57:51 +0100

    fixed missing space in seqgen parameter call

--- simulator/src/network/logic/seqgen_wrapper.py
+++ simulator/src/network/logic/seqgen_wrapper.py
@@ -1,10 +1,12 @@
 import subprocess
+import collections
+
 from experiment_model import Dataset
 
 SEQGEN_PATH = 'seq-gen'
 
 
 def simulate_msa(dataset):
-    cmd = SEQGEN_PATH + ' -mHKY -t3.0 -f0.3,0.2,0.2,0.3 -l' + str(dataset.msa_size)+'-p' + str(dataset.n_trees)+' < ' + dataset.extracted_trees_path + ' > ' + dataset.msa_path
+    cmd = SEQGEN_PATH + ' -mHKY -t3.0 -f0.3,0.2,0.2,0.3 -l' + str(dataset.msa_size)+' -p' + str(dataset.n_trees)+' < ' + dataset.extracted_trees_path + ' > ' + dataset.msa_path
     print(cmd)
     print(subprocess.getoutput(cmd))
