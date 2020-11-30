ff59f3c62235b91e64bd9ff094e7cb5ad8a51deb
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2020-11-30 22:28:17 +0100

    merging some dataframes in the plot script now

--- simulator/src/network/logic/create_plots.py
+++ simulator/src/network/logic/create_plots.py
@@ -2,6 +2,12 @@ import matplotlib
 import pandas as pd
 import os
 
+def merge_multi(dataframes, column_name):
+    merged = dataframes[0]
+    for i in range(1, len(dataframes)):
+        merged = pd.merge(merged, dataframes[i], on=column_name)
+    return merged
+
 
 def create_plots_internal(prefix, data, simulator_type, sampling_type, msa_size, likelihood_type):
     #pd.set_option('display.max_columns', None)
@@ -10,8 +16,10 @@ def create_plots_internal(prefix, data, simulator_type, sampling_type, msa_size,
     # BIC plot
     true_simulated_network_bics = filtered_data.loc[filtered_data['start_from_raxml'] == False][['name', 'bic_true']]
     inferred_network_bics = filtered_data.loc[filtered_data['start_from_raxml'] == False][['name', 'bic_inferred']]
-    inferred_network_with_raxml_bics = filtered_data.loc[filtered_data['start_from_raxml'] == True][['name', 'bic_inferred']]
+    inferred_network_with_raxml_bics = filtered_data.loc[filtered_data['start_from_raxml'] == True][['name', 'bic_inferred']].rename(columns={'bic_inferred': 'bic_inferred_with_raxml'})
     raxml_bics = filtered_data.loc[filtered_data['start_from_raxml'] == False][['name', 'bic_raxml']]
+    merged_df = merge_multi([true_simulated_network_bics, inferred_network_bics, inferred_network_with_raxml_bics, raxml_bics], 'name')
+    print(merged_df.head())
     # Logl plot
     # relative RF-distance plot
     # num near-zero raxml-ng branches plot
@@ -21,7 +29,7 @@ def create_plots_internal(prefix, data, simulator_type, sampling_type, msa_size,
 def create_plots(prefix):
     if not os.path.exists('plots_' + prefix):
         os.makedirs('plots_' + prefix)
-    data = pd.read_csv(prefix + "_results.csv", sep=',|;', index_col=False)
+    data = pd.read_csv(prefix + "_results.csv", sep=',|;', index_col=False, engine='python')
     msa_sizes = data.msa_size.unique()
     simulator_types = data.simulation_type.unique()
     sampling_types = data.sampling_type.unique()
