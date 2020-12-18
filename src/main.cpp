89de840edb9d29d1e3ed6590350274fc22b070e7
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2020-12-18 12:44:42 +0100

    score_only now also prints the newick of the given network before and after brlen and reticulation opt

--- src/main.cpp
+++ src/main.cpp
@@ -251,10 +251,16 @@ void score_only(NetraxOptions& netraxOptions, std::mt19937& rng) {
     NetraxInstance::init_annotated_network(ann_network, rng);
     NetraxInstance::optimizeModel(ann_network);
 
+    std::cout << "Initial, given network:\n";
+    std::cout << exportDebugInfo(ann_network) << "\n";
+
     double start_bic = NetraxInstance::scoreNetwork(ann_network);
     double start_logl = NetraxInstance::computeLoglikelihood(ann_network);
     std::cout << "Initial (before brlen and reticulation opt) BIC Score: " << start_bic << "\n";
     std::cout << "Initial (before brlen and reticulation opt) loglikelihood: " << start_logl << "\n";
+
+    std::cout << "Network after optimization of brlens and reticulation probs:\n";
+    std::cout << exportDebugInfo(ann_network) << "\n";
 
     NetraxInstance::optimizeBranches(ann_network);
     NetraxInstance::updateReticulationProbs(ann_network);
