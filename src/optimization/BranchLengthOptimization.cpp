cb1d66abcd5e93ebabdc4558bd3695426884f207
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2021-01-11 16:18:02 +0100

    Revert "added assertion that radius must be at least 1"
    This reverts commit 04d51ea59c635d4fe5083f76f0a4835428b4e98a.

--- src/optimization/BranchLengthOptimization.cpp
+++ src/optimization/BranchLengthOptimization.cpp
@@ -209,8 +209,6 @@ void add_branches_within_radius(AnnotatedNetwork& ann_network, int radius, size_
 
 double optimize_branches(AnnotatedNetwork &ann_network, int max_iters, int radius,
         std::unordered_set<size_t> &candidates) {
-    assert(radius >= 1);
-    assert(!candidates.empty());
     for (size_t idx : candidates) {
         add_branches_within_radius(ann_network, radius, idx, candidates);
     }
