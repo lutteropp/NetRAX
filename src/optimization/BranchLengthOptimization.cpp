585e7ae18c53c9d46e6f9635a8f7495f4bf58d82
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2021-01-11 16:00:05 +0100

    fixed implementation of add branches within radius

--- src/optimization/BranchLengthOptimization.cpp
+++ src/optimization/BranchLengthOptimization.cpp
@@ -193,23 +193,25 @@ double optimize_branch(AnnotatedNetwork &ann_network, int max_iters, int *act_it
     return logl;
 }
 
-void add_branches_within_radius(AnnotatedNetwork& ann_network, int radius, std::unordered_set<size_t>& candidates) {
-    if (radius <= 0) {
+void add_branches_within_radius(AnnotatedNetwork& ann_network, int radius, size_t pmatrix_index, std::unordered_set<size_t> &candidates) {
+    if (radius <= 0 || candidates.size() == ann_network.network.num_branches()) {
         return;
     }
-    for (size_t pmatrix_index : candidates) {
-        std::unordered_set<size_t> neighbor_indices = getNeighborPmatrixIndices(
-                        ann_network.network, ann_network.network.edges_by_index[pmatrix_index]);
-        for (size_t idx : neighbor_indices) {
+    std::unordered_set<size_t> neighbor_indices = getNeighborPmatrixIndices(
+                    ann_network.network, ann_network.network.edges_by_index[pmatrix_index]);
+    for (size_t idx : neighbor_indices) {
+        if (candidates.count(idx) == 0) {
             candidates.emplace(idx);
+            add_branches_within_radius(ann_network, radius - 1, idx, candidates);
         }
     }
-    add_branches_within_radius(ann_network, radius - 1, candidates);
 }
 
 double optimize_branches(AnnotatedNetwork &ann_network, int max_iters, int radius,
         std::unordered_set<size_t> &candidates) {
-    add_branches_within_radius(ann_network, radius, candidates);
+    for (size_t idx : candidates) {
+        add_branches_within_radius(ann_network, radius, idx, candidates);
+    }
 
     double lh_epsilon = ann_network.options.lh_epsilon;
     int act_iters = 0;
@@ -226,7 +228,7 @@ double optimize_branches(AnnotatedNetwork &ann_network, int max_iters, int radiu
         assert(start_logl == recomputed_logl);
 
         if (new_logl - old_logl > lh_epsilon) { // add all neighbors of the branch to the candidates
-            add_branches_within_radius(ann_network, radius, candidates);
+            add_branches_within_radius(ann_network, radius, pmatrix_index, candidates);
         }
         old_logl = new_logl;
     }
