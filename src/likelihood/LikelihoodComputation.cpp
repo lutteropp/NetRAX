97cda9b458cd3a0672ecc93c496e71b67b55ab15
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2021-01-11 23:48:52 +0100

    current change ensures that pmatrices only need to be updated in likelihood call if we have scaled branch lengths (because of the brlen scaler optimization)

--- src/likelihood/LikelihoodComputation.cpp
+++ src/likelihood/LikelihoodComputation.cpp
@@ -414,7 +414,10 @@ double computeLoglikelihoodNaiveUtree(AnnotatedNetwork &ann_network, int increme
 double computeLoglikelihood(AnnotatedNetwork &ann_network, int incremental, int update_pmatrices) {
     //just for debug
     //incremental = 0;
-    update_pmatrices = 1;
+
+    if (ann_network.options.brlen_linkage != PLLMOD_COMMON_BRLEN_SCALED) {
+        update_pmatrices = 0;
+    }
     return computeLoglikelihood_new(ann_network, incremental, update_pmatrices);
     //return computeLoglikelihoodNaiveUtree(ann_network, incremental, update_pmatrices);
 }
