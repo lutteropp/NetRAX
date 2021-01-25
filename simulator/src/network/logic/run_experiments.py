7375fbdaa69ce919f75d282fcc01471c5f5aaa81
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2021-01-25 14:27:14 +0100

    experiments now are using a settings struct

--- simulator/src/network/logic/run_experiments.py
+++ simulator/src/network/logic/run_experiments.py
@@ -9,6 +9,21 @@ from celine_simulator import CelineParams, simulate_network_celine_minmax
 
 from create_plots import create_plots
 
+class ExperimentSettings:
+    def __init__(self):
+        self.iterations = 1
+        self.min_taxa = 4
+        self.max_taxa = 10
+        self.min_reticulations = 1
+        self.max_reticulations = 2
+        self.sampling_types = [SamplingType.PERFECT_SAMPLING]
+        self.simulator_types = [SimulatorType.CELINE]
+        self.likelihood_types = [LikelihoodType.AVERAGE, LikelihoodType.BEST]
+        self.brlen_linkage_types = [BrlenLinkageType.LINKED]
+        self.start_types = [StartType.FROM_RAXML, StartType.RANDOM]
+        self.partition_sizes = [1000]
+
+
 def build_dataset(prefix, n_taxa, n_reticulations, msa_size, simulator_type, sampling_type, likelihood_types, brlen_linkage_types, start_types, my_id):
     if not os.path.exists('datasets_' + prefix):
         os.makedirs('datasets_' + prefix)
@@ -16,23 +31,25 @@ def build_dataset(prefix, n_taxa, n_reticulations, msa_size, simulator_type, sam
     return create_dataset_container(n_taxa, n_reticulations, msa_size, sampling_type, simulator_type, likelihood_types, brlen_linkage_types, start_types, my_id, name)
     
 
-def run_experiments(prefix, iterations, min_taxa, max_taxa, min_reticulations, max_reticulations, sampling_types, likelihood_types, brlen_linkage_types, start_types, partition_sizes):
+def run_experiments(prefix, settings):
     if not os.path.exists('datasets_' + prefix):
         os.makedirs('datasets_' + prefix)
     datasets = []
-    results = []
+
+    if SimulatorType.SARAH in settings.simulator_types:
+        raise Exception("Only SimulatorType.CELINE please")
     
     counter = dict()
-    for taxa in range(max_taxa+1):
+    for taxa in range(settings.max_taxa+1):
         counter[taxa] = defaultdict(int)
         
-    for my_id in range(iterations):
-        n_taxa, n_reticulations, newick, param_info = simulate_network_celine_minmax(min_taxa, max_taxa, min_reticulations, max_reticulations)
+    for my_id in range(settings.iterations):
+        n_taxa, n_reticulations, newick, param_info = simulate_network_celine_minmax(settings.min_taxa, settings.max_taxa, settings.min_reticulations, settings.max_reticulations)
         counter[n_taxa][n_reticulations] += 1
         n_trees = 2 ** param_info["no_of_hybrids"]
-        for partition_size in partition_sizes:
-            for sampling_type in sampling_types:
-                ds = build_dataset(prefix, n_taxa, n_reticulations, n_trees * partition_size, SimulatorType.CELINE, sampling_type, likelihood_types, brlen_linkage_types, start_types, my_id)
+        for partition_size in settings.partition_sizes:
+            for sampling_type in settings.sampling_types:
+                ds = build_dataset(prefix, n_taxa, n_reticulations, n_trees * partition_size, SimulatorType.CELINE, sampling_type, settings.likelihood_types, settings.brlen_linkage_types, settings.start_types, my_id)
                 ds.sites_per_tree = partition_size
                 ds.celine_params = param_info
                 ds.n_trees = 2 ** ds.celine_params["no_of_hybrids"]
@@ -46,8 +63,8 @@ def run_experiments(prefix, iterations, min_taxa, max_taxa, min_reticulations, m
                 simulate_msa(ds)
                 datasets.append(ds)
     
-    for i in range(max_taxa+1):
-        for j in range(max_reticulations+1):
+    for i in range(settings.max_taxa+1):
+        for j in range(settings.max_reticulations+1):
             if counter[i][j] != 0:
                 print(str(i) + ", " + str(j) + ": " + str(counter[i][j]))
     
@@ -57,64 +74,68 @@ def run_experiments(prefix, iterations, min_taxa, max_taxa, min_reticulations, m
 
 
 def run_experiments_small_tree():
-    iterations = 1
-    min_taxa = 4
-    max_taxa = 10
-    min_reticulations = 0
-    max_reticulations = 0
+    settings = ExperimentSettings()
     prefix = 'small_tree'
-    sampling_types = [SamplingType.PERFECT_SAMPLING]
-    start_types = [StartType.FROM_RAXML, StartType.RANDOM]
-    brlen_linkage_types = [BrlenLinkageType.SCALED]
-    likelihood_types = [LikelihoodType.BEST, LikelihoodType.AVERAGE]
-    partition_sizes = [500, 1000]
-    run_experiments(prefix, iterations, min_taxa, max_taxa, min_reticulations, max_reticulations, sampling_types, likelihood_types, brlen_linkage_types, start_types, partition_sizes)
+    settings.iterations = 1
+    settings.min_taxa = 4
+    settings.max_taxa = 10
+    settings.min_reticulations = 0
+    settings.max_reticulations = 0
+    settings.sampling_types = [SamplingType.PERFECT_SAMPLING]
+    settings.start_types = [StartType.FROM_RAXML, StartType.RANDOM]
+    settings.brlen_linkage_types = [BrlenLinkageType.SCALED]
+    settings.likelihood_types = [LikelihoodType.BEST, LikelihoodType.AVERAGE]
+    settings.partition_sizes = [500, 1000]
+    run_experiments(prefix, settings)
     
 
 def run_experiments_small_network():
-    iterations = 1
-    min_taxa = 4
-    max_taxa = 10
-    min_reticulations = 1
-    max_reticulations = 2
+    settings = ExperimentSettings()
     prefix = 'small_network'
-    sampling_types = [SamplingType.PERFECT_SAMPLING]
-    start_types = [StartType.FROM_RAXML]
-    brlen_linkage_types = [BrlenLinkageType.LINKED, BrlenLinkageType.SCALED, BrlenLinkageType.UNLINKED]
-    likelihood_types = [LikelihoodType.BEST, LikelihoodType.AVERAGE]
-    partition_sizes = [500, 1000]
-    run_experiments(prefix, iterations, min_taxa, max_taxa, min_reticulations, max_reticulations, sampling_types, likelihood_types, brlen_linkage_types, start_types, partition_sizes)
+    settings.iterations = 1
+    settings.min_taxa = 4
+    settings.max_taxa = 10
+    settings.min_reticulations = 1
+    settings.max_reticulations = 2
+    settings.sampling_types = [SamplingType.PERFECT_SAMPLING]
+    settings.start_types = [StartType.FROM_RAXML, StartType.RANDOM]
+    settings.brlen_linkage_types = [BrlenLinkageType.LINKED]
+    settings.likelihood_types = [LikelihoodType.BEST, LikelihoodType.AVERAGE]
+    settings.partition_sizes = [500, 1000]
+    run_experiments(prefix, settings)
     
-    
-def run_experiments_small_network_single_debug():
-    iterations = 1
-    min_taxa = 4
-    max_taxa = 4
-    min_reticulations = 1
-    max_reticulations = 1
+
+def run_experiments_small_network_single_debug(): 
+    settings = ExperimentSettings()
     prefix = 'small_network_single_debug'
-    sampling_types = [SamplingType.PERFECT_SAMPLING]
-    start_types = [StartType.FROM_RAXML]
-    brlen_linkage_types = [BrlenLinkageType.SCALED]
-    likelihood_types = [LikelihoodType.BEST, LikelihoodType.AVERAGE]
-    partition_sizes = [1000]
-    run_experiments(prefix, iterations, min_taxa, max_taxa, min_reticulations, max_reticulations, sampling_types, likelihood_types, brlen_linkage_types, start_types, partition_sizes)
+    settings.iterations = 1
+    settings.min_taxa = 4
+    settings.max_taxa = 4
+    settings.min_reticulations = 1
+    settings.max_reticulations = 1
+    settings.sampling_types = [SamplingType.PERFECT_SAMPLING]
+    settings.start_types = [StartType.FROM_RAXML]
+    settings.brlen_linkage_types = [BrlenLinkageType.LINKED]
+    settings.likelihood_types = [LikelihoodType.BEST, LikelihoodType.AVERAGE]
+    settings.partition_sizes = [1000]
+    run_experiments(prefix, settings)
     
-    
+
 def run_experiments_larger_network():
-    iterations = 1
-    min_taxa = 20
-    max_taxa = 50
-    min_reticulations = 2
-    max_reticulations = -1
+    settings = ExperimentSettings()
     prefix = 'larger_network'
-    sampling_types = [SamplingType.PERFECT_SAMPLING]
-    start_types = [StartType.FROM_RAXML, StartType.RANDOM]
-    brlen_linkage_types = [BrlenLinkageType.SCALED]
-    likelihood_types = [LikelihoodType.BEST, LikelihoodType.AVERAGE]
-    partition_sizes = [500, 1000]
-    run_experiments(prefix, iterations, min_taxa, max_taxa, min_reticulations, max_reticulations, sampling_types, likelihood_types, brlen_linkage_types, start_types, partition_sizes)
-    
+    settings.iterations = 1
+    settings.min_taxa = 20
+    settings.max_taxa = 50
+    settings.min_reticulations = 2
+    settings.max_reticulations = -1
+    settings.sampling_types = [SamplingType.PERFECT_SAMPLING]
+    settings.start_types = [StartType.FROM_RAXML, StartType.RANDOM]
+    settings.brlen_linkage_types = [BrlenLinkageType.LINKED]
+    settings.likelihood_types = [LikelihoodType.BEST, LikelihoodType.AVERAGE]
+    settings.partition_sizes = [500, 1000]
+    run_experiments(prefix, settings)
+  
     
 if __name__ == "__main__":
     #run_experiments_small_tree()
