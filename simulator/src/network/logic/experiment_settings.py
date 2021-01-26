fdb44edc7fb93b818fca13909c4c4af62ddfd0fb
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2021-01-26 02:46:02 +0100

    moved experiment settings into separate file

---
+++ simulator/src/network/logic/experiment_settings.py
@@ -0,0 +1,80 @@
+from experiment_model import SamplingType, SimulatorType, LikelihoodType, BrlenLinkageType, StartType
+
+
+class ExperimentSettings:
+    def __init__(self):
+        self.iterations = 1
+        self.min_taxa = 4
+        self.max_taxa = 10
+        self.min_reticulations = 1
+        self.max_reticulations = 2
+        self.sampling_types = [SamplingType.PERFECT_SAMPLING]
+        self.simulator_types = [SimulatorType.CELINE]
+        self.likelihood_types = [LikelihoodType.AVERAGE, LikelihoodType.BEST]
+        self.brlen_linkage_types = [BrlenLinkageType.LINKED]
+        self.start_types = [StartType.FROM_RAXML, StartType.RANDOM]
+        self.partition_sizes = [1000]
+
+
+def small_tree():
+    settings = ExperimentSettings()
+    prefix = 'small_tree'
+    settings.iterations = 1
+    settings.min_taxa = 4
+    settings.max_taxa = 10
+    settings.min_reticulations = 0
+    settings.max_reticulations = 0
+    settings.sampling_types = [SamplingType.PERFECT_SAMPLING]
+    settings.start_types = [StartType.FROM_RAXML, StartType.RANDOM]
+    settings.brlen_linkage_types = [BrlenLinkageType.SCALED]
+    settings.likelihood_types = [LikelihoodType.BEST, LikelihoodType.AVERAGE]
+    settings.partition_sizes = [500, 1000]
+    return (prefix, settings)
+
+
+def small_network():
+    settings = ExperimentSettings()
+    prefix = 'small_network'
+    settings.iterations = 1
+    settings.min_taxa = 4
+    settings.max_taxa = 10
+    settings.min_reticulations = 1
+    settings.max_reticulations = 2
+    settings.sampling_types = [SamplingType.PERFECT_SAMPLING]
+    settings.start_types = [StartType.FROM_RAXML, StartType.RANDOM]
+    settings.brlen_linkage_types = [BrlenLinkageType.LINKED]
+    settings.likelihood_types = [LikelihoodType.BEST, LikelihoodType.AVERAGE]
+    settings.partition_sizes = [500, 1000]
+    return (prefix, settings)
+
+
+def small_network_single_debug(): 
+    settings = ExperimentSettings()
+    prefix = 'small_network_single_debug'
+    settings.iterations = 1
+    settings.min_taxa = 4
+    settings.max_taxa = 4
+    settings.min_reticulations = 1
+    settings.max_reticulations = 1
+    settings.sampling_types = [SamplingType.PERFECT_SAMPLING]
+    settings.start_types = [StartType.FROM_RAXML]
+    settings.brlen_linkage_types = [BrlenLinkageType.LINKED]
+    settings.likelihood_types = [LikelihoodType.BEST, LikelihoodType.AVERAGE]
+    settings.partition_sizes = [1000]
+    return (prefix, settings)
+
+
+def larger_network():
+    settings = ExperimentSettings()
+    prefix = 'larger_network'
+    settings.iterations = 1
+    settings.min_taxa = 20
+    settings.max_taxa = 50
+    settings.min_reticulations = 2
+    settings.max_reticulations = -1
+    settings.sampling_types = [SamplingType.PERFECT_SAMPLING]
+    settings.start_types = [StartType.FROM_RAXML, StartType.RANDOM]
+    settings.brlen_linkage_types = [BrlenLinkageType.LINKED]
+    settings.likelihood_types = [LikelihoodType.BEST, LikelihoodType.AVERAGE]
+    settings.partition_sizes = [500, 1000]
+    return (prefix, settings)
--- simulator/src/network/logic/run_experiments.py
+++ simulator/src/network/logic/run_experiments.py
@@ -10,19 +10,7 @@ from celine_simulator import CelineParams, simulate_network_celine_minmax
 from create_plots import create_plots
 import pandas as pd
 
-class ExperimentSettings:
-    def __init__(self):
-        self.iterations = 1
-        self.min_taxa = 4
-        self.max_taxa = 10
-        self.min_reticulations = 1
-        self.max_reticulations = 2
-        self.sampling_types = [SamplingType.PERFECT_SAMPLING]
-        self.simulator_types = [SimulatorType.CELINE]
-        self.likelihood_types = [LikelihoodType.AVERAGE, LikelihoodType.BEST]
-        self.brlen_linkage_types = [BrlenLinkageType.LINKED]
-        self.start_types = [StartType.FROM_RAXML, StartType.RANDOM]
-        self.partition_sizes = [1000]
+from experiment_settings import ExperimentSettings, small_tree, larger_network, small_network, small_network_single_debug
 
 
 def build_dataset(prefix, n_taxa, n_reticulations, msa_size, simulator_type, sampling_type, likelihood_types, brlen_linkage_types, start_types, my_id):
@@ -82,84 +70,20 @@ def run_experiments(prefix, settings):
     datasets = simulate_datasets(prefix, settings)
     run_inference_and_evaluate(datasets)
     write_results_to_csv(datasets, prefix + "_results.csv")
-    #create_plots(prefix)
 
 
-def run_experiments_multi(prefix, settings, iterations):
+def run_multi(prefix, settings, iterations):
     local_csv_paths = []
     for it in range(len(iterations)):
         local_prefix = prefix + "_" + str(it)
         run_experiments(local_prefix, settings)
         local_csv_paths.append(local_prefix + "_results.csv")
     merge_csvs(local_csv_paths, prefix + "_results.csv")
-    
-
-def run_experiments_small_tree():
-    settings = ExperimentSettings()
-    prefix = 'small_tree'
-    settings.iterations = 1
-    settings.min_taxa = 4
-    settings.max_taxa = 10
-    settings.min_reticulations = 0
-    settings.max_reticulations = 0
-    settings.sampling_types = [SamplingType.PERFECT_SAMPLING]
-    settings.start_types = [StartType.FROM_RAXML, StartType.RANDOM]
-    settings.brlen_linkage_types = [BrlenLinkageType.SCALED]
-    settings.likelihood_types = [LikelihoodType.BEST, LikelihoodType.AVERAGE]
-    settings.partition_sizes = [500, 1000]
-    run_experiments(prefix, settings)
-    
-
-def run_experiments_small_network():
-    settings = ExperimentSettings()
-    prefix = 'small_network'
-    settings.iterations = 1
-    settings.min_taxa = 4
-    settings.max_taxa = 10
-    settings.min_reticulations = 1
-    settings.max_reticulations = 2
-    settings.sampling_types = [SamplingType.PERFECT_SAMPLING]
-    settings.start_types = [StartType.FROM_RAXML, StartType.RANDOM]
-    settings.brlen_linkage_types = [BrlenLinkageType.LINKED]
-    settings.likelihood_types = [LikelihoodType.BEST, LikelihoodType.AVERAGE]
-    settings.partition_sizes = [500, 1000]
-    run_experiments(prefix, settings)
-    
-
-def run_experiments_small_network_single_debug(): 
-    settings = ExperimentSettings()
-    prefix = 'small_network_single_debug'
-    settings.iterations = 1
-    settings.min_taxa = 4
-    settings.max_taxa = 4
-    settings.min_reticulations = 1
-    settings.max_reticulations = 1
-    settings.sampling_types = [SamplingType.PERFECT_SAMPLING]
-    settings.start_types = [StartType.FROM_RAXML]
-    settings.brlen_linkage_types = [BrlenLinkageType.LINKED]
-    settings.likelihood_types = [LikelihoodType.BEST, LikelihoodType.AVERAGE]
-    settings.partition_sizes = [1000]
-    run_experiments(prefix, settings)
-    
-
-def run_experiments_larger_network():
-    settings = ExperimentSettings()
-    prefix = 'larger_network'
-    settings.iterations = 1
-    settings.min_taxa = 20
-    settings.max_taxa = 50
-    settings.min_reticulations = 2
-    settings.max_reticulations = -1
-    settings.sampling_types = [SamplingType.PERFECT_SAMPLING]
-    settings.start_types = [StartType.FROM_RAXML, StartType.RANDOM]
-    settings.brlen_linkage_types = [BrlenLinkageType.LINKED]
-    settings.likelihood_types = [LikelihoodType.BEST, LikelihoodType.AVERAGE]
-    settings.partition_sizes = [500, 1000]
-    run_experiments(prefix, settings)
+    create_plots(prefix)
   
     
 if __name__ == "__main__":
-    #run_experiments_small_tree()
-    run_experiments_small_network()
-    #run_experiments_larger_network()
-    #run_experiments_small_network_single_debug()
+    #run_multi(*small_tree(), 2)
+    run_multi(*small_network(), 2)
+    #run_multi(*larger_network(), 2)
+    #run_multi(*small_network_single_debug(), 1)
