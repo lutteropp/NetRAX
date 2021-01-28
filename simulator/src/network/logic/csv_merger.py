004f65300907955dd88030852ce224ab22cee76b
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2021-01-28 14:14:12 +0100

    fixed csv merger

--- simulator/src/network/logic/csv_merger.py
+++ simulator/src/network/logic/csv_merger.py
@@ -6,7 +6,8 @@ import os.path
 def merge_csvs(inpaths, outpath):
     for p in inpaths:
         if not os.path.isfile(p):
-            raise Exception("Not a file: " + p)
+            print(p)
+            raise Exception("Not a file")
     combined_csv = pd.concat([pd.read_csv(f) for f in inpaths])
     combined_csv.to_csv(outpath, index=False, encoding='utf-8-sig')
 
@@ -16,7 +17,6 @@ def postprocess_merge(prefix, iterations):
     for it in range(iterations):
         local_prefix = prefix + "_" + str(it)
         local_csv_paths.append(local_prefix + "_results.csv")
-    print(local_csv_paths)
     merge_csvs(local_csv_paths, prefix + "_results.csv")
 
 
