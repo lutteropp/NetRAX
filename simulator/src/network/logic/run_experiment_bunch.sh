4e3744f9b5ac1a0ae6bf9ad72ffce2a894e35989
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2021-01-28 12:12:49 +0100

    fixed wrong spaces in bash script

--- simulator/src/network/logic/run_experiment_bunch.sh
+++ simulator/src/network/logic/run_experiment_bunch.sh
@@ -1,32 +1,32 @@
 #!/usr/bin/env bash
 
 if [ $# -lt 2 ]; then
-    echo "Illegal number of parameters. Usage: run_experiment_bunch.sh PREFIX ITERATIONS [--no_random]"
+    echo "Illegal number of parameters. Usage: sh run_experiment_bunch.sh PREFIX ITERATIONS [--no_random]"
     exit 2
 fi
 
-PREFIX = $1
-ITERATIONS = $2
-SAMPLING_TYPES = "SamplingType.PERFECT_SAMPLING"
-START_TYPES = "StartType.FROM_RAXML StartType.RANDOM"
-BRLEN_LINKAGE_TYPES = "BrlenLinkageType.LINKED"
-LIKELIHOOD_TYPES = "LikelihoodType.AVERAGE LikelihoodType.BEST"
-PARTITION_SIZES = "50 100"
+PREFIX=$1
+ITERATIONS=$2
+SAMPLING_TYPES="SamplingType.PERFECT_SAMPLING"
+START_TYPES="StartType.FROM_RAXML StartType.RANDOM"
+BRLEN_LINKAGE_TYPES="BrlenLinkageType.LINKED"
+LIKELIHOOD_TYPES="LikelihoodType.AVERAGE LikelihoodType.BEST"
+PARTITION_SIZES="50 100"
 
 if [ $# == 3]; then
-    if [[ $3 == "--no_random" ]]; then
-        START_TYPES = "StartType.FROM_RAXML"
+    if [ $3 == "--no_random" ]; then
+        START_TYPES="StartType.FROM_RAXML"
     else
-        echo "Illegal third argument. Usage: run_experiment_bunch.sh PREFIX ITERATIONS [--no_random]"
+        echo "Illegal third argument. Usage: sh run_experiment_bunch.sh PREFIX ITERATIONS [--no_random]"
         exit 2
     fi
 fi
 
 case ${PREFIX} in
-"small_network") MIN_TAXA = 4; MAX_TAXA = 10; MIN_RETICULATIONS = 1; MAX_RETICULATIONS = 2;;
-"small_network_debug") MIN_TAXA = 4; MAX_TAXA = 4; MIN_RETICULATIONS = 1; MAX_RETICULATIONS = 2;;
-"larger_network") MIN_TAXA = 20; MAX_TAXA = 50; MIN_RETICULATIONS = 2; MAX_RETICULATIONS = -1;;
-"small_tree") MIN_TAXA = 4; MAX_TAXA = 10; MIN_RETICULATIONS = 0; MAX_RETICULATIONS = 0;;
+"small_network") MIN_TAXA=4; MAX_TAXA=10; MIN_RETICULATIONS=1; MAX_RETICULATIONS=2;;
+"small_network_debug") MIN_TAXA=4; MAX_TAXA=4; MIN_RETICULATIONS=1; MAX_RETICULATIONS=2;;
+"larger_network") MIN_TAXA=20; MAX_TAXA=50; MIN_RETICULATIONS=2; MAX_RETICULATIONS=-1;;
+"small_tree") MIN_TAXA=4; MAX_TAXA=10; MIN_RETICULATIONS=0; MAX_RETICULATIONS=0;;
 *) echo "Unknown prefix"; exit 2;;
 esac
 
