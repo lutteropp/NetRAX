374e21950119a275a38e24947e3e9c05c2572b16
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2021-01-28 11:50:28 +0100

    added some default parameters to bash script

--- simulator/src/network/logic/run_experiment_bunch.sh
+++ simulator/src/network/logic/run_experiment_bunch.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-ITERATIONS = $2
-PREFIX = $1
+ITERATIONS = 2
+PREFIX = "small_network"
 
-SAMPLING_TYPES = ""
-START_TYPES = ""
-BRLEN_LINKAGE_TYPES = ""
-LIKELIHOOD_TYPES = ""
-PARTITION_SIZES = ""
-MIN_TAXA = ""
-MAX_TAXA = ""
-MIN_RETICULATIONS = ""
-MAX_RETICULATIONS = ""
+SAMPLING_TYPES = "SamplingType.PERFECT_SAMPLING"
+START_TYPES = "StartType.FROM_RAXML StartType.RANDOM"
+BRLEN_LINKAGE_TYPES = "BrlenLinkageType.LINKED"
+LIKELIHOOD_TYPES = "LikelihoodType.AVERAGE LikelihoodType.BEST"
+PARTITION_SIZES = "50 100"
+MIN_TAXA = "4"
+MAX_TAXA = "10"
+MIN_RETICULATIONS = "1"
+MAX_RETICULATIONS = "2"
 
 for ((i = 0; i < ${ITERATIONS}; i++)); do
     python3 run_experiments.py --prefix ${PREFIX}_${i} --iterations 1 --sampling_types ${SAMPLING_TYPES} --start_types ${START_TYPES} --brlen_linkage_types ${BRLEN_LINKAGE_TYPES} --likelihood_types ${LIKELIHOOD_TYPES} --partition_sizes ${PARTITION_SIZES} --min_taxa ${MIN_TAXA} --max_taxa ${MAX_TAXA} --min_reticulations ${MIN_RETICULATIONS} --max_reticulations ${MAX_RETICULATIONS} &
