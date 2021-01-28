a594e1270637bcb98e9f9c9a00890f03b0de9c80
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2021-01-28 11:59:55 +0100

    added some basic argument parsing

--- simulator/src/network/logic/run_experiment_bunch.sh
+++ simulator/src/network/logic/run_experiment_bunch.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-ITERATIONS = 2
-PREFIX = "small_network"
-
+PREFIX = $1
+ITERATIONS = $2
 SAMPLING_TYPES = "SamplingType.PERFECT_SAMPLING"
 START_TYPES = "StartType.FROM_RAXML StartType.RANDOM"
 BRLEN_LINKAGE_TYPES = "BrlenLinkageType.LINKED"
 LIKELIHOOD_TYPES = "LikelihoodType.AVERAGE LikelihoodType.BEST"
 PARTITION_SIZES = "50 100"
-MIN_TAXA = "4"
-MAX_TAXA = "10"
-MIN_RETICULATIONS = "1"
-MAX_RETICULATIONS = "2"
+
+case ${PREFIX} in
+"small_network") MIN_TAXA = 4; MAX_TAXA = 10; MIN_RETICULATIONS = 1; MAX_RETICULATIONS = 2;;
+"small_tree") MIN_TAXA = 4; MAX_TAXA = 10; MIN_RETICULATIONS = 0; MAX_RETICULATIONS = 0;;
+esac
 
 for ((i = 0; i < ${ITERATIONS}; i++)); do
     python3 run_experiments.py --prefix ${PREFIX}_${i} --iterations 1 --sampling_types ${SAMPLING_TYPES} --start_types ${START_TYPES} --brlen_linkage_types ${BRLEN_LINKAGE_TYPES} --likelihood_types ${LIKELIHOOD_TYPES} --partition_sizes ${PARTITION_SIZES} --min_taxa ${MIN_TAXA} --max_taxa ${MAX_TAXA} --min_reticulations ${MIN_RETICULATIONS} --max_reticulations ${MAX_RETICULATIONS} &
