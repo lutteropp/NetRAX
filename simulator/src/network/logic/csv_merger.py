aa214e37da0971570aec97366e73a1f631326f32
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2021-01-28 14:10:32 +0100

    fixed syntax error in csv merger

--- simulator/src/network/logic/csv_merger.py
+++ simulator/src/network/logic/csv_merger.py
@@ -1,8 +1,12 @@
 import pandas as pd
 import argparse
+import os.path
 
 
 def merge_csvs(inpaths, outpath):
+    for p in inpaths:
+        if not os.path.isfile(p):
+            raise Exception("Not a file: " + p)
     combined_csv = pd.concat([pd.read_csv(f) for f in inpaths])
     combined_csv.to_csv(outpath, index=False, encoding='utf-8-sig')
 
@@ -12,6 +16,7 @@ def postprocess_merge(iterations, prefix):
     for it in range(iterations):
         local_prefix = prefix + "_" + str(it)
         local_csv_paths.append(local_prefix + "_results.csv")
+    print(local_csv_paths)
     merge_csvs(local_csv_paths, prefix + "_results.csv")
 
 
