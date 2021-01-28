67c64856f73c55f669895a54eed7917ae5c13fc1
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2021-01-28 14:32:29 +0100

    run_experiments_per_bunch now also gets iterations_per_bunch parameter

--- simulator/src/network/logic/run_experiments_larger_bunch.sh
+++ simulator/src/network/logic/run_experiments_larger_bunch.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
 
-ITERATIONS_PER_BUNCH=2
+USAGE="Usage: sh run_experiments_larger_bunch.sh PREFIX BUNCHES ITERATIONS_PER_BUNCH MIN_TAXA MAX_TAXA MIN_RETICULATIONS MAX_RETICULATIONS [no_random]"
 
-USAGE="Usage: sh run_experiments_larger_bunch.sh PREFIX BUNCHES MIN_TAXA MAX_TAXA MIN_RETICULATIONS MAX_RETICULATIONS [no_random]"
-
-if [ $# -lt 6 ]; then
+if [ $# -lt 7 ]; then
     echo "Illegal number of parameters. Usage: ${USAGE}"
     exit 2
 fi
 
 PREFIX=$1
 BUNCHES=$2
-MIN_TAXA=$3
-MAX_TAXA=$4
-MIN_RETICULATIONS=$5
-MAX_RETICULATIONS=$6
+ITERATIONS_PER_BUNCH=$3
+MIN_TAXA=$4
+MAX_TAXA=$5
+MIN_RETICULATIONS=$6
+MAX_RETICULATIONS=$7
 
 SAMPLING_TYPES="PERFECT_SAMPLING"
 START_TYPES="FROM_RAXML RANDOM"
@@ -24,11 +23,11 @@ PARTITION_SIZES="50 100"
 
 i=0
 while [ $i -lt ${BUNCHES} ]; do
-    if [ $# -eq 7 ]; then
-        if [ $7=="no_random" ]; then
+    if [ $# -eq 8 ]; then
+        if [ $8=="no_random" ]; then
             sh run_experiments_bunch.sh ${PREFIX}_${i} ${ITERATIONS_PER_BUNCH} ${MIN_TAXA} ${MAX_TAXA} ${MIN_RETICULATIONS} ${MAX_RETICULATIONS} no_random
         else
-            echo "Unknown argument: ${7}"
+            echo "Unknown argument: ${8}"
             exit 2
         fi
     else
