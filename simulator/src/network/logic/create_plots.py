1e65a30dc1cbd6c84794febf4a73b690e86b3200
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2021-02-13 00:12:56 +0100

    Added some percentages in result evaluation report

--- simulator/src/network/logic/create_plots.py
+++ simulator/src/network/logic/create_plots.py
@@ -19,10 +19,20 @@ def cm_to_inch(value):
 
 
 def bic_logl_stats(df):
-    print("Inferred BIC better or equal: " + str(len(df[df['bic_inferred'] <= df['bic_true']])))
-    print("Inferred BIC worse: " + str(len(df[df['bic_inferred'] > df['bic_true']])))
-    print("Inferred loglh better or equal: " + str(len(df[df['logl_inferred'] >= df['logl_true']])))
-    print("Inferred loglh worse: " + str(len(df[df['logl_inferred'] < df['logl_true']])))
+    bic_better_or_equal_abs = len(df[df['bic_inferred'] <= df['bic_true']])
+    bic_better_or_equal_perc = float(bic_better_or_equal_abs * 100) / len(df)
+    bic_worse_abs = len(df[df['bic_inferred'] > df['bic_true']])
+    bic_worse_perc = float(bic_worse_abs * 100) / len(df)
+
+    logl_better_or_equal_abs = len(df[df['logl_inferred'] >= df['logl_true']])
+    logl_better_or_equal_perc = float(logl_better_or_equal_abs * 100) / len(df)
+    logl_worse_abs = len(df[df['logl_inferred'] < df['logl_true']])
+    logl_worse_perc = float(logl_worse_abs * 100) / len(df)
+
+    print("Inferred BIC better or equal: " + str(bic_better_or_equal_abs) + ", which is " + "%.2f" % bic_better_or_equal_perc + " %")
+    print("Inferred BIC worse: " + str(bic_worse_abs) + ", which is " + "%.2f" % bic_worse_perc + " %")
+    print("Inferred logl better or equal: " + str(logl_better_or_equal_abs) + ", which is " + "%.2f" % logl_better_or_equal_perc + " %")
+    print("Inferred logl worse: " + str(logl_worse_abs) + ", which is " + "%.2f" % logl_worse_perc + " %")
     fig, axes = plt.subplots(1, 2, constrained_layout=True)
     fig.suptitle("BIC and Loglikelihood Statistics")
     df['bic_diff'].plot.hist(bins=100, alpha=0.5, title='(bic_true - bic_inferred) / bic_true\n value >0 means inferred BIC was better', ax=axes[0])
@@ -30,12 +40,18 @@ def bic_logl_stats(df):
     
     
 def reticulation_stats(df):
-    print("Inferred n_reticulations less: " + str(len(df[df['n_reticulations_inferred'] < df['n_reticulations']])))
-    print("Inferred n_reticulations equal: " + str(len(df[df['n_reticulations_inferred'] == df['n_reticulations']])))
-    print("Inferred n_reticulations more: " + str(len(df[df['n_reticulations_inferred'] > df['n_reticulations']])))
+    reticulations_less_abs = len(df[df['n_reticulations_inferred'] < df['n_reticulations']])
+    reticulations_less_perc = float(reticulations_less_abs * 100) / len(df)
+    reticulations_equal_abs = len(df[df['n_reticulations_inferred'] == df['n_reticulations']])
+    reticulations_equal_perc = float(reticulations_equal_abs * 100) / len(df)
+    reticulations_more_abs = len(df[df['n_reticulations_inferred'] > df['n_reticulations']])
+    reticulations_more_perc = float(reticulations_more_abs * 100) / len(df)
+    print("Inferred n_reticulations less: " + str(reticulations_less_abs) + ", which is " + "%.2f" % reticulations_less_perc + " %")
+    print("Inferred n_reticulations equal: " + str(reticulations_equal_abs) + ", which is " + "%.2f" % reticulations_equal_perc + " %")
+    print("Inferred n_reticulations more: " + str(reticulations_more_abs) + ", which is " + "%.2f" % reticulations_more_perc + " %")
 
     
-def weirdness_stats(df):
+def plot_weirdness_stats(df):
     plt.figure()
     fig, axes = plt.subplots(1, 2)
     fig.suptitle("Network Weirdness Statistics")
@@ -62,17 +78,27 @@ def plots_setup():
     plt.rc('ytick', labelsize='x-small')
 
 
+def plot_dataset_size(df):
+    plt.figure()
+    fig, axes = plt.subplots(1, 3)
+    fig.suptitle("Simulated Dataset Stats")
+    df['n_taxa'].plot.hist(bins=10, alpha=0.5, title='Number of taxa', ax=axes[0])
+    df['near_zero_branches_raxml'].plot.hist(bins=10, alpha=0.5, title='Number of reticulations', ax=axes[1])
+    df['msa_size'].plot.hist(bins=10, alpha=0.5, title='MSA size', ax=axes[2])
+    plt.tight_layout()
+
+
 def show_stats(df):
-    print(df.columns)
+    plot_dataset_size(df)
+    plot_weirdness_stats(df)
 
 
 def show_plots(df):
-    plots_setup()
     bic_logl_stats(df)
     print("")
     reticulation_stats(df)
     print("")
-    weirdness_stats(df)
+    
     print("")
     distances(df)
 
