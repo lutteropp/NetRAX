e63545e344fc0dfded78cee31b017bee53fe4944
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2021-03-13 12:37:04 +0100

    made processNodeImproved direction-agnostic by passing it a parentOrSingleChild pointer

--- src/likelihood/LikelihoodComputation.cpp
+++ src/likelihood/LikelihoodComputation.cpp
@@ -433,7 +433,7 @@ unsigned int processNodeImprovedTwoChildren(AnnotatedNetwork& ann_network, unsig
     return num_trees_added;
 }
 
-void processNodeImproved(AnnotatedNetwork& ann_network, unsigned int partition_idx, int incremental, ClvRangeInfo &clvInfo, ScaleBufferRangeInfo &scaleBufferInfo, Node* node) {
+void processNodeImproved(AnnotatedNetwork& ann_network, unsigned int partition_idx, int incremental, ClvRangeInfo &clvInfo, ScaleBufferRangeInfo &scaleBufferInfo, Node* node, Node* parentOrSingleChild, bool append = false) {
     if (node->clv_index < ann_network.network.num_tips()) {
         //assert(ann_network.fake_treeinfo->clv_valid[partition_idx][node->clv_index]);
         return;
@@ -443,9 +443,16 @@ void processNodeImproved(AnnotatedNetwork& ann_network, unsigned int partition_i
         return;
     }
 
-    displayed_trees.num_active_displayed_trees = 0;
+    if (!append) {
+        displayed_trees.num_active_displayed_trees = 0;
+    }
 
-    std::vector<Node*> children = getChildren(ann_network.network, node);
+    std::vector<Node*> children;
+    if (node->getType() == NodeType::RETICULATION_NODE) {
+        children.emplace_back(parentOrSingleChild);
+    } else {
+        children = getChildrenIgnoreDirections(ann_network.network, node, parentOrSingleChild);
+    }
     if (children.size() == 1) { // we are at a reticulation node
         assert(node->getType() == NodeType::RETICULATION_NODE);
         processNodeImprovedSingleChild(ann_network, partition_idx, clvInfo, scaleBufferInfo, node, children[0]);
@@ -471,7 +478,13 @@ void processPartitionImproved(AnnotatedNetwork& ann_network, unsigned int partit
     
     for (size_t i = 0; i < ann_network.travbuffer.size(); ++i) {
         Node* actNode = ann_network.travbuffer[i];
-        processNodeImproved(ann_network, partition_idx, incremental, clvInfo, scaleBufferInfo, actNode);
+        Node* parentOrSingleChild;
+        if (actNode->getType() == NodeType::RETICULATION_NODE) {
+            parentOrSingleChild = getReticulationChild(ann_network.network, actNode);
+        } else {
+            parentOrSingleChild = getActiveParent(ann_network.network, actNode);
+        }
+        processNodeImproved(ann_network, partition_idx, incremental, clvInfo, scaleBufferInfo, actNode, parentOrSingleChild);
     }
 
     /*std::cout << "\nloglikelihood for trees at each node, for partition " << partition_idx << ":\n";
@@ -638,7 +651,65 @@ std::vector<Node*> getParentPointers(AnnotatedNetwork& ann_network, const std::v
     return parent;
 }
 
+std::vector<Node*> getParentPointers(AnnotatedNetwork& ann_network, Node* virtual_root) {
+    assert(virtual_root);
+    std::vector<Node*> parent(ann_network.network.num_nodes(), nullptr);
+    parent[virtual_root->clv_index] = virtual_root;
+    std::queue<Node*> q;
+    
+    q.emplace(virtual_root);
+    while (!q.empty()) {
+        Node* actNode = q.front();
+        q.pop();
+        std::vector<Node*> neighbors = getActiveNeighbors(ann_network.network, actNode);
+        for (size_t i = 0; i < neighbors.size(); ++i) {
+            Node* neigh = neighbors[i];
+            if (!parent[neigh->clv_index]) { // neigh was not already processed
+                q.emplace(neigh);
+                parent[neigh->clv_index] = actNode;
+            }
+        }
+    }
+    parent[virtual_root->clv_index] = nullptr;
+    return parent;
+}
+
 void updateCLVsVirtualRerootTrees(AnnotatedNetwork& ann_network, Node* old_virtual_root, Node* new_virtual_root) {
+    if (old_virtual_root == new_virtual_root) {
+        return;
+    }
+    assert(old_virtual_root);
+    assert(new_virtual_root);
+    
+    // 1.) Find the first reticulation node retNode on any path from old_virtual_root to new_virtual_root
+    setReticulationParents(ann_network.network, 0);
+    std::vector<Node*> parent = getParentPointers(ann_network, new_virtual_root);
+    std::vector<Node*> path = getPathToVirtualRoot(ann_network, old_virtual_root, new_virtual_root, parent);
+    std::vector<Node*> sharedPath;
+    Node* firstReticulationNode = nullptr;
+    for (size_t i = 0; i < path.size(); ++i) {
+        sharedPath.emplace_back(path[i]);
+        if (path[i]->getType() == NodeType::RETICULATION_NODE) {
+            firstReticulationNode = path[i];
+            break;
+        }
+    }
+    // 2.) update CLVs for the nodes on the path from old_virtual_root to retNode. This is the part that is shared among all paths!
+    for (size_t i = 0; i < sharedPath.size(); ++i) {
+        for (size_t p = 0; p < ann_network.fake_treeinfo->partition_count; ++p) {
+            ClvRangeInfo clvInfo = get_clv_range(ann_network.fake_treeinfo->partitions[p]);
+            ScaleBufferRangeInfo scaleBufferInfo = get_scale_buffer_range(ann_network.fake_treeinfo->partitions[p]);
+            processNodeImproved(ann_network, p, 0, clvInfo, scaleBufferInfo, sharedPath[i], parent[sharedPath[i]->clv_index]);
+        }
+    }
+    
+ 
+    // 3.) for all paths from retNode to new_virtual_root:
+    //     3.1) Collect the reticulation nodes encountered on the path
+    //     3.2) update CLVs on that path, 
+
+    // (make sure that instead of using hard-coded reticulation parents here, we check on a branch-basis like how the reticulation state has to be for the branch to be active)
+
     throw std::runtime_error("Not implemented yet");
 }
 
