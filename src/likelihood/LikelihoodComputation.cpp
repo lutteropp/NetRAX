45085dbfd441bd358c2bb5c64b6fa6185994fb68
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2021-04-20 01:12:53 +0200

    fixed assertion

--- src/likelihood/LikelihoodComputation.cpp
+++ src/likelihood/LikelihoodComputation.cpp
@@ -145,10 +145,10 @@ void computeDisplayedTreeLoglikelihood(AnnotatedNetwork& ann_network, DisplayedT
     /* sum up likelihood from all threads */
     if (ann_network.fake_treeinfo->parallel_reduce_cb)
     {   
-        std::cout << "I am thread " << ParallelContext::local_proc_id() << " and I have these partition loglikelihoods before Allreduce:\n";
+        /*std::cout << "I am thread " << ParallelContext::local_proc_id() << " and I have these partition loglikelihoods before Allreduce:\n";
         for (size_t p = 0; p < ann_network.fake_treeinfo->partition_count; ++p) {
             std::cout << " partition " << p << " has logl: " << treeAtRoot.treeLoglData.tree_partition_logl[p] << "\n";
-        }
+        }*/
 
         ann_network.fake_treeinfo->parallel_reduce_cb(ann_network.fake_treeinfo->parallel_context,
                                     treeAtRoot.treeLoglData.tree_partition_logl.data(),
@@ -436,9 +436,9 @@ unsigned int processNodeImprovedTwoChildren(AnnotatedNetwork& ann_network, Node*
                     double* right_clv = rightTree.clv_vector[partition_idx];
                     unsigned int* right_scaler = rightTree.scale_buffer[partition_idx];
                     if (node->clv_index == ann_network.network.root->clv_index) {
-                        std::cout << "I am thread " << ParallelContext::local_proc_id() << " in 2 children mode and I am updating a root clv for partition " << partition_idx << "\n";
+                        //std::cout << "I am thread " << ParallelContext::local_proc_id() << " in 2 children mode and I am updating a root clv for partition " << partition_idx << "\n";
                     }
-                    std::cout << "I am at node with clv index " << node->clv_index << "\n";
+                    //std::cout << "I am at node with clv index " << node->clv_index << "\n";
                     assert(leftTree.isTip || !single_clv_is_all_zeros(ann_network.partition_clv_ranges[partition_idx], left_clv));
                     assert(rightTree.isTip || !single_clv_is_all_zeros(ann_network.partition_clv_ranges[partition_idx], right_clv));
                     pll_update_partials_single(partition, &op_both, 1, parent_clv, left_clv, right_clv, parent_scaler, left_scaler, right_scaler);
@@ -520,8 +520,8 @@ unsigned int processNodeImprovedTwoChildren(AnnotatedNetwork& ann_network, Node*
                 unsigned int* left_scaler = rightTree.scale_buffer[partition_idx];
                 double* right_clv = partition->clv[fake_clv_index];
                 unsigned int* right_scaler = nullptr;
-                assert(!single_clv_is_all_zeros(ann_network.partition_clv_ranges[partition_idx], left_clv));
-                assert(rightTree.isTip || !single_clv_is_all_zeros(ann_network.partition_clv_ranges[partition_idx], right_clv));
+                assert(rightTree.isTip || !single_clv_is_all_zeros(ann_network.partition_clv_ranges[partition_idx], left_clv));
+                assert(!single_clv_is_all_zeros(ann_network.partition_clv_ranges[partition_idx], right_clv));
                 pll_update_partials_single(partition, &op_right_only, 1, parent_clv, left_clv, right_clv, parent_scaler, left_scaler, right_scaler);
                 assert(!single_clv_is_all_zeros(ann_network.partition_clv_ranges[partition_idx], parent_clv));
             }
