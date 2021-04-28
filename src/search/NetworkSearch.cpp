888951c7c27c2648fff9ca6465659f7d89ad7cbc
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2021-04-28 16:53:54 +0200

    when given a user-specified start network, first try to add arc insertions

--- src/search/NetworkSearch.cpp
+++ src/search/NetworkSearch.cpp
@@ -1105,7 +1105,7 @@ void wavesearch(AnnotatedNetwork& ann_network, BestNetworkData* bestNetworkData,
     //std::cout << "Initial network is:\n" << toExtendedNewick(ann_network) << "\n\n";
 
     if (!ann_network.options.start_network_file.empty()) { // don't waste time trying to first horizontally optimize the user-given start network
-        wavesearch_internal(ann_network, bestNetworkData, {MoveType::ArcRemovalMove, MoveType::DeltaPlusMove}, start_state_to_reuse, best_state_to_reuse, &best_score, start_time, silent);
+        wavesearch_internal(ann_network, bestNetworkData, {MoveType::DeltaPlusMove}, start_state_to_reuse, best_state_to_reuse, &best_score, start_time, silent);
         //applyBestCandidate(ann_network, possibleDeltaPlusMoves(ann_network), &best_score, bestNetworkData);
     }
 
