1bb2f71b5dd8592ebc3d0f8a51f9e5fd4afcf4fd
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2021-04-29 02:44:40 +0200

    created submission script from running raxml-ng on big empirical dataset

---
+++ simulator/src/network/logic/submit_raxml_big_empirical.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH -o raxng_%j.out
+#SBATCH -B 2:8:1
+#SBATCH --ntasks-per-node=1
+##SBATCH --ntasks-per-socket=1
+#SBATCH --cpus-per-task=16
+#SBATCH --hint=compute_bound
+#SBATCH -t 24:00:00
+#SBATCH -N 20
+#SBATCH -n 20
+ 
+source /etc/profile.d/modules.sh
+module load gompi/2019a
+
+root=/home/luttersh/NetRAX/simulator/src/network/logic/data
+raxng=/home/luttersh/NetRAX/libs/raxml-ng/bin/raxml-ng-mpi
+ali=$root/datasets_big_empirical/merged_genes_msa.txt.raxml.rba
+model=$root/datasets_big_empirical/merged_genes_partitions.txt
+outdir=$root/datasets_big_empirical
+
+mkdir -p $outdir
+
+mpirun $raxng --search --msa $ali --seed 42 --prefix $outdir --threads 16 --workers 1 --redo
