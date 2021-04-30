16a2d20b4f26bf0a9b72e96b45650f682fc6c358
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2021-04-30 19:14:02 +0200

    raxml-ng submission script for big empirical dataset now doesn't run 20 tree searches in parallel

--- simulator/src/network/logic/submit_raxml_big_empirical.sh
+++ simulator/src/network/logic/submit_raxml_big_empirical.sh
@@ -20,4 +20,4 @@ outdir=$root/datasets_big_empirical
 
 mkdir -p $outdir
 
-mpirun $raxng --search --msa $ali --seed 42 --prefix $outdir --redo --site-repeats off
+mpirun $raxng --search --msa $ali --seed 42 --prefix $outdir --redo --site-repeats off --workers 1
