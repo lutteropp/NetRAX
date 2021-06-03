7ff6c83b627cf8986952ad530a22df27dcaa71f2
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2021-06-03 18:01:57 +0200

    changed add extra moves policy

--- src/search/CandidateSelection.cpp
+++ src/search/CandidateSelection.cpp
@@ -853,23 +853,18 @@ std::vector<Move> fastIterationsMode(AnnotatedNetwork &ann_network,
       oldCandidates = candidates;
 
       if (!hadBadReticulationAfterInsertingArc) {
-        double cand_bic = prefilterCandidates(ann_network, candidates, silent);
-        if (cand_bic >= old_score) {  // only consider more possible moves if
-                                      // the old candidates don't
-          // bring it anymore...
-          std::vector<Node *> start_nodes =
-              gatherStartNodes(ann_network, chosenMove);
-          std::vector<Move> moreMoves =
-              possibleMoves(ann_network, type, start_nodes, rspr1_present,
-                            delta_plus_present, 0, best_max_distance);
-          if (ParallelContext::master_rank() &&
-              ParallelContext::master_thread()) {
-            std::cout << "Adding " << moreMoves.size() << " candidates to the "
-                      << candidates.size() << " previous ones.\n ";
-          }
-          candidates.insert(std::end(candidates), std::begin(moreMoves),
-                            std::end(moreMoves));
+        std::vector<Node *> start_nodes =
+            gatherStartNodes(ann_network, chosenMove);
+        std::vector<Move> moreMoves =
+            possibleMoves(ann_network, type, start_nodes, rspr1_present,
+                          delta_plus_present, 0, best_max_distance);
+        if (ParallelContext::master_rank() &&
+            ParallelContext::master_thread()) {
+          std::cout << "Adding " << moreMoves.size() << " candidates to the "
+                    << candidates.size() << " previous ones.\n ";
         }
+        candidates.insert(std::end(candidates), std::begin(moreMoves),
+                          std::end(moreMoves));
       }
 
       if (candidates.empty()) {  // no old candidates to reuse. Thus,
