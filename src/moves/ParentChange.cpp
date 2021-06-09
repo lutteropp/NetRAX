ad8350eaf169d763eb0b44c488e6f7ca45f1e9b5
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2021-06-09 23:15:58 +0200

    throw not implemented yet runtime error in ParentChange move functions for now

--- src/moves/ParentChange.cpp
+++ src/moves/ParentChange.cpp
@@ -2,80 +2,74 @@
 
 namespace netrax {
 
-bool checkSanityParentChange(AnnotatedNetwork &ann_network, const Move &move);
+bool checkSanityParentChange(AnnotatedNetwork &ann_network, const Move &move) {
+  throw std::runtime_error("Not implemented yet");
+}
 
 std::vector<Move> possibleMovesParentChange(AnnotatedNetwork &ann_network,
                                             const Edge *edge, bool noDeltaPlus,
-                                            int min_radius, int max_radius);
+                                            int min_radius, int max_radius) {
+  throw std::runtime_error("Not implemented yet");
+}
+
 std::vector<Move> possibleMovesParentChange(AnnotatedNetwork &ann_network,
                                             bool noDeltaPlus, int min_radius,
-                                            int max_radius);
+                                            int max_radius) {
+  throw std::runtime_error("Not implemented yet");
+}
 
 std::vector<Move> possibleMovesParentChange(AnnotatedNetwork &ann_network,
                                             const Node *node, bool noDeltaPlus,
-                                            int min_radius, int max_radius);
+                                            int min_radius, int max_radius) {
+  throw std::runtime_error("Not implemented yet");
+}
 
 std::vector<Move> possibleMovesParentChange(
     AnnotatedNetwork &ann_network, const std::vector<Node *> &start_nodes,
-    bool noDeltaPlus, int min_radius, int max_radius);
+    bool noDeltaPlus, int min_radius, int max_radius) {
+  throw std::runtime_error("Not implemented yet");
+}
 
 std::vector<Move> possibleMovesParentChange(
     AnnotatedNetwork &ann_network, const std::vector<Edge *> &start_edges,
-    bool noDeltaPlus, int min_radius, int max_radius);
+    bool noDeltaPlus, int min_radius, int max_radius) {
+  throw std::runtime_error("Not implemented yet");
+}
 
 void performMoveParentChange(AnnotatedNetwork &ann_network, Move &move) {
-  assert(isParentChange(move.moveType));
-
-  Move removal(move);
-  removal.moveType = MoveType::ArcRemovalMove;
-  performMove(ann_network, removal);
-
-  Move insertion(move);
-  insertion.moveType = MoveType::ArcInsertionMove;
-  updateMove(ann_network.network, removal, insertion);
-  performMove(ann_network, insertion);
-
-  move.arcRemovalData = removal.arcRemovalData;
-  move.arcInsertionData = insertion.arcInsertionData;
-
-  move.remapped_clv_indices = insertion.remapped_clv_indices;
-  move.remapped_pmatrix_indices = insertion.remapped_pmatrix_indices;
-  move.remapped_reticulation_indices = insertion.remapped_reticulation_indices;
+  throw std::runtime_error("Not implemented yet");
 }
 
 void undoMoveParentChange(AnnotatedNetwork &ann_network, Move &move) {
-  assert(isParentChange(move.moveType));
-
-  Move insertion(move);
-  insertion.moveType = MoveType::ArcInsertionMove;
-  performMove(ann_network, insertion);
-
-  Move removal(move);
-  removal.moveType = MoveType::ArcRemovalMove;
-  updateMove(ann_network.network, insertion, removal);
-  performMove(ann_network, removal);
-
-  move.arcRemovalData = removal.arcRemovalData;
-  move.arcInsertionData = insertion.arcInsertionData;
-
-  move.remapped_clv_indices = removal.remapped_clv_indices;
-  move.remapped_pmatrix_indices = removal.remapped_pmatrix_indices;
-  move.remapped_reticulation_indices = removal.remapped_reticulation_indices;
+  throw std::runtime_error("Not implemented yet");
 }
 
-std::string toStringParentChange(const Move &move);
+std::string toStringParentChange(const Move &move) {
+  throw std::runtime_error("Not implemented yet");
+}
 
 std::unordered_set<size_t> brlenOptCandidatesParentChange(
-    AnnotatedNetwork &ann_network, const Move &move);
+    AnnotatedNetwork &ann_network, const Move &move) {
+  throw std::runtime_error("Not implemented yet");
+}
 
 std::unordered_set<size_t> brlenOptCandidatesUndoParentChange(
-    AnnotatedNetwork &ann_network, const Move &move);
+    AnnotatedNetwork &ann_network, const Move &move) {
+  throw std::runtime_error("Not implemented yet");
+}
 
-Move randomMoveParentChange(AnnotatedNetwork &ann_network);
+Move randomMoveParentChange(AnnotatedNetwork &ann_network) {
+  throw std::runtime_error("Not implemented yet");
+}
 
 void updateMoveClvIndexParentChange(Move &move, size_t old_clv_index,
-                                    size_t new_clv_index, bool undo);
+                                    size_t new_clv_index, bool undo) {
+  throw std::runtime_error("Not implemented yet");
+}
+
 void updateMovePmatrixIndexParentChange(Move &move, size_t old_pmatrix_index,
-                                        size_t new_pmatrix_index, bool undo);
+                                        size_t new_pmatrix_index, bool undo) {
+  throw std::runtime_error("Not implemented yet");
+}
 
 }  // namespace netrax
