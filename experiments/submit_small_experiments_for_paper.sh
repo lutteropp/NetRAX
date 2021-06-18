5545a11e1d99db2a06336e7b03aab0b831423802
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2021-06-18 22:06:54 +0200

    fixed submission script

--- experiments/submit_small_experiments_for_paper.sh
+++ experiments/submit_small_experiments_for_paper.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-names=("t_25_change_reticulation_prob", "t_25_change_reticulation_count", "t_25_change_brlen_scaler", "t_25_unpartitioned")
+names=(
+    "t_25_change_reticulation_prob"
+    "t_25_change_reticulation_count"
+    "t_25_change_brlen_scaler"
+    "t_25_unpartitioned"
+    )
 
-for name in ${names[@]}
+for name in ${names[*]}
 do
     sbatch -N 1 -n 1 submit_experiments_haswell.sh $name $name 1
 done
