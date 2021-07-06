fae6b6e18288b0f6f297cf856c21ac98524509e7
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2021-07-06 10:07:59 +0200

    fixed yet another import

--- experiments/src/scramble_partitions_experiment.py
+++ experiments/src/scramble_partitions_experiment.py
@@ -2,8 +2,7 @@ from scramble_partitions import scramble_partitions, write_partitions
 from experiment_settings import exp_standard
 from netrax_wrapper import infer_networks
 from evaluate_experiments import run_inference_and_evaluate, write_results_to_csv
-from run_experiments import simulate_network_celine_fixed_nonweird
-from dataset_builder import build_dataset
+from run_experiments import simulate_network_celine_fixed_nonweird, build_dataset
 import argparse
 import copy
 
