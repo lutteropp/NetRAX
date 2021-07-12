5d9d4160fad2ae81afbf1ea831512ba20ceeb66e
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2021-07-12 11:26:20 +0200

    created raxml-ng submission script for subsampled empirical dataset

---
+++ experiments/submit_raxml_big_empirical_species.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH -o raxng_%j.out
+#SBATCH -B 2:8:1
+#SBATCH --ntasks-per-node=1
+##SBATCH --ntasks-per-socket=1
+#SBATCH --cpus-per-task=16
+#SBATCH --hint=compute_bound
+#SBATCH -t 24:00:00
+#SBATCH -N 20
+#SBATCH -n 20
+ 
+module purge
+module load gompi/2019a
+
+root=/home/luttersh/NetRAX/experiments/data
+raxng=/home/luttersh/raxml-ng/bin/raxml-ng-mpi
+ali=$root/datasets_big_empirical/merged_genes_species_msa.txt.raxml.rba
+model=$root/datasets_big_empirical/merged_genes_species_partitions.txt
+outdir=$root/datasets_big_empirical
+
+mkdir -p $outdir
+
+mpirun $raxng --search --msa $ali --seed 42 --prefix $outdir --redo --site-repeats off --workers 1
