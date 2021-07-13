db832620eb44025042de6589cf34491384c27eea
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2021-07-13 14:06:58 +0200

    submission script for proper experiments, for PhD laptop for now as there I can easily check what's going on

---
+++ experiments/submit_proper_experiments_for_paper_phd_laptop.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sh submit_experiments_phd_laptop.sh "t_10_r_1_standard_random" 50
+sh submit_experiments_phd_laptop.sh "t_20_r_2_standard_random" 50
+sh submit_experiments_phd_laptop.sh "t_30_r_3_standard_random" 50
+sh submit_experiments_phd_laptop.sh "t_20_r_1_change_reticulation_prob" 50
+sh submit_experiments_phd_laptop.sh "t_20_r_1_unpartitioned" 50
