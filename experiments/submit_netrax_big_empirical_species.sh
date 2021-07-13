1ca8309b9405696c90a350303fdf2e8e86bc74f2
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2021-07-13 10:48:43 +0200

    haswell submission script for continuing netrax run on subsampled empirical dataset

---
+++ experiments/submit_netrax_big_empirical_species.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH -o netrax_%j.out
+#SBATCH -B 2:8:1
+#SBATCH --ntasks-per-node=1
+##SBATCH --ntasks-per-socket=1
+#SBATCH --cpus-per-task=16
+#SBATCH --hint=compute_bound
+#SBATCH -t 24:00:00
+#SBATCH -N 20
+#SBATCH -n 20
+ 
+module purge
+module load gompi/2019a
+
+root=/home/luttersh/NetRAX/experiments/subsampled_empirical
+netrax=/home/luttersh/NetRAX/bin/netrax
+ali=$root/merged_genes_species_msa.txt.raxml.rba
+model=$root/merged_genes_species_partitions.txt
+raxtree=$root/start_network.txt
+outdir=$root
+
+mkdir -p $outdir
+
+mpiexec $netrax --msa $ali --model $model --seed 42 --output $outdir/merged_genes_species_inferred_network.nw --best_displayed_tree_variant --brlen linked --start_network $raxtree --good_start
