fd7031f0923f561b384947fb516cf6309337653c
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2021-07-14 18:03:01 +0200

    also echo the name

--- experiments/submit_proper_experiments_for_paper_haswell.sh
+++ experiments/submit_proper_experiments_for_paper_haswell.sh
@@ -3,17 +3,22 @@
 for i in {0..49}
 do
     name="A_10_1_run_$i"
+    echo $name
     sbatch -N 1 -n 1 submit_experiments_haswell.sh "t_10_r_1_standard_random" "$name" 1
 
     name="A_20_2_run_$i"
+    echo $name
     sbatch -N 1 -n 1 submit_experiments_haswell.sh "t_20_r_2_standard_random" "$name" 1
 
     name="A_30_3_run_$i"
+    echo $name
     sbatch -N 1 -n 1 submit_experiments_haswell.sh "t_30_r_3_standard_random" "$name" 1
 
     name="B_20_1_run_$i"
+    echo $name
     sbatch -N 1 -n 1 submit_experiments_haswell.sh "t_20_r_1_change_reticulation_prob" "$name" 1
 
     name="C_20_1_run_$i"
+    echo $name
     sbatch -N 1 -n 1 submit_experiments_haswell.sh "t_20_r_1_unpartitioned" "$name" 1
 done
