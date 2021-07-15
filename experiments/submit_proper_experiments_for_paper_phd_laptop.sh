4e0293d38458d919f96e8af7637da543ef85ba9e
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2021-07-15 11:10:50 +0200

    first run the faster experiments on jerry

--- experiments/submit_proper_experiments_for_paper_phd_laptop.sh
+++ experiments/submit_proper_experiments_for_paper_phd_laptop.sh
@@ -7,24 +7,39 @@ do
     name="A_10_1_run_$i"
     echo $name
     bash submit_experiments_phd_laptop.sh "t_10_r_1_standard_random" "$name" 1
+done
 
+for i in {0..49}
+do
     name="A_20_2_run_$i"
     echo $name
     bash submit_experiments_phd_laptop.sh "t_20_r_2_standard_random" "$name" 1
+done
 
-    name="A_30_3_run_$i"
-    echo $name
-    bash submit_experiments_phd_laptop.sh "t_30_r_3_standard_random" "$name" 1
-
-    name="A_40_4_run_$i"
-    echo $name
-    bash submit_experiments_phd_laptop.sh "t_40_r_4_standard_random" "$name" 1
-
+for i in {0..49}
+do
     name="B_20_1_run_$i"
     echo $name
     bash submit_experiments_phd_laptop.sh "t_20_r_1_change_reticulation_prob" "$name" 1
+done
 
+for i in {0..49}
+do
     name="C_20_1_run_$i"
     echo $name
     bash submit_experiments_phd_laptop.sh "t_20_r_1_unpartitioned" "$name" 1
+done
+
+for i in {0..49}
+do
+    name="A_30_3_run_$i"
+    echo $name
+    bash submit_experiments_phd_laptop.sh "t_30_r_3_standard_random" "$name" 1
+done
+
+for i in {0..49}
+do
+    name="A_40_4_run_$i"
+    echo $name
+    bash submit_experiments_phd_laptop.sh "t_40_r_4_standard_random" "$name" 1
 done
