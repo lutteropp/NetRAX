1451d4d67d4f83481e8f1ef5e8b577d1a7c2e451
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2021-07-16 09:54:38 +0200

    added an optional nice-to-have experiment

--- experiments/submit_proper_experiments_for_paper_haswell.sh
+++ experiments/submit_proper_experiments_for_paper_haswell.sh
@@ -14,10 +14,6 @@ do
     echo $name
     sbatch -N 1 -n 1 submit_experiments_haswell.sh "t_30_r_3_standard_random" "$name" 1
 
-    name="A_40_4_run_$i"
-    echo $name
-    sbatch -N 1 -n 1 submit_experiments_haswell.sh "t_40_r_4_standard_random" "$name" 1
-
     name="B_20_1_run_$i"
     echo $name
     sbatch -N 1 -n 1 submit_experiments_haswell.sh "t_20_r_1_change_reticulation_prob" "$name" 1
@@ -25,4 +21,28 @@ do
     name="C_20_1_run_$i"
     echo $name
     sbatch -N 1 -n 1 submit_experiments_haswell.sh "t_20_r_1_unpartitioned" "$name" 1
+
+    name="A_40_4_run_$i"
+    echo $name
+    sbatch -N 1 -n 1 submit_experiments_haswell.sh "t_40_r_4_standard_random" "$name" 1
+
+    name="A_norandom_40_1_run_$i"
+    echo $name
+    sbatch -N 1 -n 1 submit_experiments_haswell.sh "t_40_r_1_standard" "$name" 1
+
+    name="A_norandom_40_2_run_$i"
+    echo $name
+    sbatch -N 1 -n 1 submit_experiments_haswell.sh "t_40_r_2_standard" "$name" 1
+
+    name="A_norandom_40_3_run_$i"
+    echo $name
+    sbatch -N 1 -n 1 submit_experiments_haswell.sh "t_40_r_3_standard" "$name" 1
+
+    name="A_norandom_40_4_run_$i"
+    echo $name
+    sbatch -N 1 -n 1 submit_experiments_haswell.sh "t_40_r_4_standard" "$name" 1
+
+    name="A_norandom_40_5_run_$i"
+    echo $name
+    sbatch -N 1 -n 1 submit_experiments_haswell.sh "t_40_r_5_standard" "$name" 1
 done
