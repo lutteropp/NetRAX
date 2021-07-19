3b784db3b3a7cde29f2e87655ac52539288ade53
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2021-07-19 10:33:37 +0200

    removed the slow ones

--- experiments/submit_proper_experiments_for_paper_haswell_missing_iteration.sh
+++ experiments/submit_proper_experiments_for_paper_haswell_missing_iteration.sh
@@ -21,10 +21,6 @@ name="C_20_1_run_$i"
 echo $name
 sbatch -N 1 -n 1 submit_experiments_haswell.sh "t_20_r_1_unpartitioned" "$name" 1
 
-name="A_40_4_run_$i"
-echo $name
-sbatch -N 1 -n 1 submit_experiments_haswell.sh "t_40_r_4_standard_random" "$name" 1
-
 name="A_norandom_40_1_run_$i"
 echo $name
 sbatch -N 1 -n 1 submit_experiments_haswell.sh "t_40_r_1_standard" "$name" 1
