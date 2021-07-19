f927c3d199e0a77edf58f1d0c549c1986c542220
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2021-07-19 14:08:39 +0200

    removed rogue code

--- experiments/src/repair_network_scores.py
+++ experiments/src/repair_network_scores.py
@@ -67,12 +67,6 @@ def recompute_network_scores(prefix):
         logl_inferred.append(act_logl_inferred)
         aic_inferred.append(act_aic_inferred)
         aicc_inferred.append(act_aicc_inferred)
-        
-        n_equal_tree_pairs.append(n_equal)
-        if n_pairs > 0:
-            true_network_weirdness.append(float(n_equal)/n_pairs)
-        else:
-            true_network_weirdness.append(0.0)
             
         print(row["name"])
         
