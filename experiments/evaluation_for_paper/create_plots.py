22f89f2647b9adc52d17ba6d08962e9eeb7c4713
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2021-07-21 10:54:29 +0200

    print table to latex, added good/okay/bad statistics

--- experiments/evaluation_for_paper/create_plots.py
+++ experiments/evaluation_for_paper/create_plots.py
@@ -11,8 +11,12 @@ sns.set(style="darkgrid")
 def import_dataframe(filename):
     df = pd.read_csv(filename)
     df['bic_diff'] = (df['bic_true'] - df['bic_inferred'])
+    df['aic_diff'] = (df['aic_true'] - df['aic_inferred'])
+    df['aicc_diff'] = (df['aicc_true'] - df['aicc_inferred'])
     df['logl_diff'] = (df['logl_true'] - df['logl_inferred'])
     df['bic_diff_relative'] = (df['bic_true'] - df['bic_inferred']) / df['bic_true']
+    df['aic_diff_relative'] = (df['aic_true'] - df['aic_inferred']) / df['aic_true']
+    df['aicc_diff_relative'] = (df['aicc_true'] - df['aicc_inferred']) / df['aicc_true']
     df['logl_diff_relative'] = (df['logl_true'] - df['logl_inferred']) / df['logl_true']
     df['msa_patterns_relative'] = df['msa_patterns'] / df['msa_size']
     return df
@@ -30,10 +34,30 @@ def generate_ascii_table(df):
 def report_bic_better_or_equal(df):
     cnt = len(df[df['bic_inferred'] <= df['bic_true']])
     return str(cnt) + " (" + "%.2f" % (float(cnt*100)/len(df)) + " %)"
+    
+    
+def report_aic_better_or_equal(df):
+    cnt = len(df[df['aic_inferred'] <= df['aic_true']])
+    return str(cnt) + " (" + "%.2f" % (float(cnt*100)/len(df)) + " %)"
 
+
+def report_aicc_better_or_equal(df):
+    cnt = len(df[df['aicc_inferred'] <= df['aicc_true']])
+    return str(cnt) + " (" + "%.2f" % (float(cnt*100)/len(df)) + " %)"
+    
 
 def report_bic_worse(df):
     cnt = len(df[df['bic_inferred'] > df['bic_true']])
+    return str(cnt) + " (" + "%.2f" % (float(cnt*100)/len(df)) + " %)"
+    
+    
+def report_aic_worse(df):
+    cnt = len(df[df['aic_inferred'] > df['aic_true']])
+    return str(cnt) + " (" + "%.2f" % (float(cnt*100)/len(df)) + " %)"
+    
+    
+def report_aicc_worse(df):
+    cnt = len(df[df['aicc_inferred'] > df['aicc_true']])
     return str(cnt) + " (" + "%.2f" % (float(cnt*100)/len(df)) + " %)"
 
 
@@ -60,16 +84,43 @@ def report_reticulations_equal(df):
 def report_reticulations_more(df):
     cnt = len(df[df['n_reticulations_inferred'] > df['n_reticulations']])
     return str(cnt) + " (" + "%.2f" % (float(cnt*100)/len(df)) + " %)"
+    
+
+def report_unrooted_softwired_distance_zero(df):
+    cnt = len(df[df['unrooted_softwired_network_distance'] == 0])
+    return str(cnt) + " (" + "%.2f" % (float(cnt*100)/len(df)) + " %)"
+    
+    
+def report_good_result(df):
+    cnt = len(df[(df['bic_inferred'] <= df['bic_true']) | (df['unrooted_softwired_network_distance'] == 0)])
+    return str(cnt) + " (" + "%.2f" % (float(cnt*100)/len(df)) + " %)"
+    
+    
+def report_okay_result(df):
+    cnt = len(df[(df['bic_inferred'] > df['bic_true']) & (df['unrooted_softwired_network_distance'] > 0) & (df['n_reticulations_inferred'] == df['n_reticulations'])])
+    return str(cnt) + " (" + "%.2f" % (float(cnt*100)/len(df)) + " %)"
+    
+
+def report_bad_result(df):
+    cnt = len(df[(df['bic_inferred'] > df['bic_true']) & (df['unrooted_softwired_network_distance'] > 0) & (df['n_reticulations_inferred'] != df['n_reticulations'])])
+    return str(cnt) + " (" + "%.2f" % (float(cnt*100)/len(df)) + " %)"
 
 
 def plot_relative_quality_stats(prefix, df):
     plt.figure()
     fig, axes = plt.subplots(2, 2)
-    fig.suptitle(prefix.split("_results.csv")[0] + " - " + "Relative Quality Statistics")
-    df['bic_diff_relative'].plot.hist(bins=100, alpha=0.5, title='Relative BIC difference (>0 means better)\n (bic_true - bic_inferred) / bic_true', ax=axes[0][0])
-    df['logl_diff_relative'].plot.hist(bins=100, alpha=0.5, title='Relative loglh difference (<0 means better)\n (logl_true - logl_inferred) / logl_true', ax=axes[0][1])
-    df['bic_diff'].plot.hist(bins=100, alpha=0.5, title='Absolute BIC difference (>0 means better)\n (bic_true - bic_inferred)', ax=axes[1][0])
-    df['logl_diff'].plot.hist(bins=100, alpha=0.5, title='Absolute loglh difference (<0 means better)\n (logl_true - logl_inferred)', ax=axes[1][1])
+    fig.suptitle(prefix + " - " + "Relative Quality Statistics")
+    df['logl_diff_relative'].plot.hist(bins=100, alpha=0.5, title='Relative loglh difference (<0 means better)\n (logl_true - logl_inferred) / logl_true', ax=axes[0][0])
+    df['bic_diff_relative'].plot.hist(bins=100, alpha=0.5, title='Relative BIC difference (>0 means better)\n (bic_true - bic_inferred) / bic_true', ax=axes[0][1])
+    
+    df['aic_diff_relative'].plot.hist(bins=100, alpha=0.5, title='Relative AIC difference (>0 means better)\n (aic_true - aic_inferred) / aic_true', ax=axes[1][0])
+    df['aicc_diff_relative'].plot.hist(bins=100, alpha=0.5, title='Relative AICc difference (>0 means better)\n (aicc_true - aicc_inferred) / aicc_true', ax=axes[1][1])
+    
+    #df['logl_diff'].plot.hist(bins=100, alpha=0.5, title='Absolute loglh difference (<0 means better)\n (logl_true - logl_inferred)', ax=axes[2][0])
+    #df['bic_diff'].plot.hist(bins=100, alpha=0.5, title='Absolute BIC difference (>0 means better)\n (bic_true - bic_inferred)', ax=axes[2][1])
+    
+    #df['aic_diff'].plot.hist(bins=100, alpha=0.5, title='Absolute AIC difference (>0 means better)\n (aic_true - aic_inferred)', ax=axes[3][0])
+    #df['aicc_diff'].plot.hist(bins=100, alpha=0.5, title='Absolute AICc difference (>0 means better)\n (aicc_true - aicc_inferred)', ax=axes[3][1])
     plt.tight_layout()
     plt.savefig(prefix + '_relative_quality_stats.png')
     plt.show()
@@ -78,22 +129,31 @@ def plot_relative_quality_stats(prefix, df):
 def quality_stats(prefix, df):
     df_likelihood_average = df.query('likelihood_type == "AVERAGE"')
     df_likelihood_best = df.query('likelihood_type == "BEST"')
-    data = [['Inferred BIC better or equal',report_bic_better_or_equal(df_likelihood_average),report_bic_better_or_equal(df_likelihood_best),report_bic_better_or_equal(df)],
-            ['Inferred BIC worse',report_bic_worse(df_likelihood_average),report_bic_worse(df_likelihood_best),report_bic_worse(df)],
-            ['Inferred logl better or equal',report_logl_better_or_equal(df_likelihood_average),report_logl_better_or_equal(df_likelihood_best),report_logl_better_or_equal(df)],
-            ['Inferred logl worse', report_logl_worse(df_likelihood_average), report_logl_worse(df_likelihood_best),report_logl_worse(df)],
-            ['Inferred n_reticulations less', report_reticulations_less(df_likelihood_average),report_reticulations_less(df_likelihood_best),report_reticulations_less(df)],
-            ['Inferred n_reticulations equal', report_reticulations_equal(df_likelihood_average),report_reticulations_equal(df_likelihood_best),report_reticulations_equal(df)],
-            ['Inferred n_reticulations more', report_reticulations_more(df_likelihood_average),report_reticulations_more(df_likelihood_best),report_reticulations_more(df)]]
-    data_df = pd.DataFrame(data, columns=['', 'LikelihoodType.AVERAGE', 'LikelihoodType.BEST', 'Overall'])
+    data = [['Inferred BIC better or equal',report_bic_better_or_equal(df_likelihood_average),report_bic_better_or_equal(df_likelihood_best)],
+            ['Inferred AIC better or equal',report_aic_better_or_equal(df_likelihood_average),report_aic_better_or_equal(df_likelihood_best)],
+            ['Inferred AICc better or equal',report_aicc_better_or_equal(df_likelihood_average),report_aicc_better_or_equal(df_likelihood_best)],
+            ['Inferred BIC worse',report_bic_worse(df_likelihood_average),report_bic_worse(df_likelihood_best)],
+            ['Inferred AIC worse',report_aic_worse(df_likelihood_average),report_aic_worse(df_likelihood_best)],
+            ['Inferred AICc worse',report_aicc_worse(df_likelihood_average),report_aicc_worse(df_likelihood_best)],
+            ['Inferred logl better or equal',report_logl_better_or_equal(df_likelihood_average),report_logl_better_or_equal(df_likelihood_best)],
+            ['Inferred logl worse', report_logl_worse(df_likelihood_average), report_logl_worse(df_likelihood_best)],
+            ['Inferred n_reticulations less', report_reticulations_less(df_likelihood_average),report_reticulations_less(df_likelihood_best)],
+            ['Inferred n_reticulations equal', report_reticulations_equal(df_likelihood_average),report_reticulations_equal(df_likelihood_best)],
+            ['Inferred n_reticulations more', report_reticulations_more(df_likelihood_average),report_reticulations_more(df_likelihood_best)],
+            ['Unrooted softwired distance zero', report_unrooted_softwired_distance_zero(df_likelihood_average),report_unrooted_softwired_distance_zero(df_likelihood_best)],
+            ['Good result', report_good_result(df_likelihood_average),report_good_result(df_likelihood_best)],
+            ['Okay result', report_okay_result(df_likelihood_average),report_okay_result(df_likelihood_best)],
+            ['Bad result', report_bad_result(df_likelihood_average),report_bad_result(df_likelihood_best)]]
+    data_df = pd.DataFrame(data, columns=[prefix, 'LikelihoodType.AVERAGE', 'LikelihoodType.BEST'])
     generate_ascii_table(data_df)
+    print(data_df.to_latex())
     plot_relative_quality_stats(prefix, df)
     
     
 def plot_weirdness_stats(prefix, df):
     plt.figure()
     fig, axes = plt.subplots(1, 2)
-    fig.suptitle(prefix.split("_results.csv")[0] + " - " + "Network Weirdness Statistics")
+    fig.suptitle(prefix + " - " + "Network Weirdness Statistics")
     df['true_network_weirdness'].plot.hist(bins=10, alpha=0.5, range=(0,1), title='True network weirdness', ax=axes[0])
     df['near_zero_branches_raxml'].plot.hist(bins=10, alpha=0.5, title='Near-zero branches raxml', ax=axes[1])
     plt.tight_layout()
@@ -121,7 +181,7 @@ def distances_dendroscope(df):
 def distances_netrax(prefix, df):
     plt.figure()
     fig, axes = plt.subplots(1, 3, constrained_layout=True)
-    fig.suptitle(prefix.split("_results.csv")[0] + " - " + "Unrooted Topological Network Distances")
+    fig.suptitle(prefix + " - " + "Unrooted Topological Network Distances")
     df['unrooted_softwired_network_distance'].plot.hist(bins=10, alpha=0.5, range=(0,1), title='Unrooted softwired cluster distance', ax=axes[0])
     df['unrooted_hardwired_network_distance'].plot.hist(bins=10, alpha=0.5, range=(0,1), title='Unrooted hardwired cluster distance', ax=axes[1])
     df['unrooted_displayed_trees_distance'].plot.hist(bins=10, alpha=0.5, range=(0,1), title='Unrooted displayed trees distance', ax=axes[2])
@@ -130,7 +190,7 @@ def distances_netrax(prefix, df):
 
     plt.figure()
     fig, axes = plt.subplots(2, 3, constrained_layout=True)
-    fig.suptitle(prefix.split("_results.csv")[0] + " - " + "Rooted Topological Network Distances")
+    fig.suptitle(prefix + " - " + "Rooted Topological Network Distances")
     df['rooted_softwired_network_distance'].plot.hist(bins=10, alpha=0.5, range=(0,1), title='Rooted softwired cluster distance', ax=axes[0,0])
     df['rooted_hardwired_network_distance'].plot.hist(bins=10, alpha=0.5, range=(0,1), title='Rooted hardwired cluster distance', ax=axes[0,1])
     df['rooted_displayed_trees_distance'].plot.hist(bins=10, alpha=0.5, range=(0,1), title='Rooted displayed trees distance', ax=axes[0,2])
@@ -152,12 +212,13 @@ def plot_dataset_size(prefix, df):
     print("Total number of datasets: " + str(len(df)))
     plt.figure()
     fig, axes = plt.subplots(2, 3)
-    fig.suptitle(prefix.split("_results.csv")[0] + " - " + "Simulated Dataset Stats")
+    fig.suptitle(prefix + " - " + "Simulated Dataset Stats")
     df['n_taxa'].plot.hist(bins=10, alpha=0.5, title='Number of taxa', ax=axes[0][0])
     df['n_reticulations'].plot.hist(bins=10, alpha=0.5, title='Number of reticulations', ax=axes[0][1])
     df['msa_size'].plot.hist(bins=10, alpha=0.5, title='MSA size', ax=axes[0][2])
     df['msa_patterns'].plot.hist(bins=10, alpha=0.5, title='MSA patterns (absolute count)', ax=axes[1][0])
     df['msa_patterns_relative'].plot.hist(bins=10, alpha=0.5, range=(0,1), title='MSA patterns (fraction)', ax=axes[1][1])
+    df['near_zero_branches_raxml'].plot.hist(bins=10, alpha=0.5, title='Near-zero branches raxml', ax=axes[1][2])
     plt.tight_layout()
     plt.savefig(prefix + '_dataset_size.png')
     plt.show()
@@ -165,7 +226,7 @@ def plot_dataset_size(prefix, df):
 
 def show_stats(prefix, df):
     plot_dataset_size(prefix, df)
-    plot_weirdness_stats(prefix, df)
+    #plot_weirdness_stats(prefix, df)
     
 
 def show_pattern_quality_effects(prefix, df):
