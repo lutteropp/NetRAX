b6e4e200c0ed1f90855f6ea10a9f47b9c54def01
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2021-07-27 14:52:23 +0200

    fixed typo

--- experiments/submit_mpi_runtime_experiment_haswell_manually.sh
+++ experiments/submit_mpi_runtime_experiment_haswell_manually.sh
@@ -37,13 +37,13 @@ echo "iteration,n_procs,likelihood_type,runtime_in_seconds" > mpi_experiment_res
 for procs in {64,32,16,8,4,2,1}
 do
     start_best=`date +%s`
-    mpiexec -np $procs /home/luttersh/NetRAX/bin/netrax --msa data/datasets_mpi_runtime_${ITERATION}/0_0_msa.txt --output data/datasets_mpi_runtime_${ITERATION}/${procs}_0_BEST_LINKED_FROM_RAXML_inferred_network.nw --model data/datasets_mpi_runtime_${ITERATION}/0_0_partitions.txt --best_displayed_tree_variant --start_network data/datasets_mpi_runtime_${ITERATION}/0_0.raxml.bestTree --good_start --brlen linked --seed 42
+    mpiexec -np ${procs} /home/luttersh/NetRAX/bin/netrax --msa data/datasets_mpi_runtime_${ITERATION}/0_0_msa.txt --output data/datasets_mpi_runtime_${ITERATION}/${procs}_0_BEST_LINKED_FROM_RAXML_inferred_network.nw --model data/datasets_mpi_runtime_${ITERATION}/0_0_partitions.txt --best_displayed_tree_variant --start_network data/datasets_mpi_runtime_${ITERATION}/0_0.raxml.bestTree --good_start --brlen linked --seed 42
     end_best=`date +%s`
     runtime_best=`expr ${end_best} - ${start_best}`
     echo ${ITERATION}","${procs}",BEST,"$runtime_best >> mpi_experiment_results_${ITERATION}.csv
 
     start_avg=`date +%s`
-    mpiexec -np $procs /home/luttersh/NetRAX/bin/netrax --msa data/datasets_mpi_runtime_${ITERATION}/0_0_msa.txt --output data/datasets_mpi_runtime_${ITERATION}/${procs}_0_AVERAGE_LINKED_FROM_RAXML_inferred_network.nw --model data/datasets_mpi_runtime_${ITERATION}/0_0_partitions.txt --average_displayed_tree_variant --start_network data/datasets_mpi_runtime_${ITERATION}/0_0.raxml.bestTree --good_start --brlen linked --seed 42
+    mpiexec -np ${procs} /home/luttersh/NetRAX/bin/netrax --msa data/datasets_mpi_runtime_${ITERATION}/0_0_msa.txt --output data/datasets_mpi_runtime_${ITERATION}/${procs}_0_AVERAGE_LINKED_FROM_RAXML_inferred_network.nw --model data/datasets_mpi_runtime_${ITERATION}/0_0_partitions.txt --average_displayed_tree_variant --start_network data/datasets_mpi_runtime_${ITERATION}/0_0.raxml.bestTree --good_start --brlen linked --seed 42
     end_avg=`date +%s`
     runtime_avg=`expr ${end_avg} - ${start_avg}`
     echo ${ITERATION}","${procs}",AVERAGE,"$runtime_avg >> mpi_experiment_results_${ITERATION}.csv
