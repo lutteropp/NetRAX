0934eb5460bc37d42613bd79b9bd6999e76eabb6
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2021-08-08 23:05:45 +0200

    changed the sets of trees to use

--- experiments/assemble_snakes/run_netrax_on_snakes.sh
+++ experiments/assemble_snakes/run_netrax_on_snakes.sh
@@ -13,13 +13,13 @@ model=$root/snakes_partitions.txt
 besttree=$root/snakes.raxml.bestTree
 mltrees=$root/snakes.raxml.mlTrees
 mltrees_unique=$root/snakes.raxml.mlTrees.unique
-badtrees=$root/snakes.raxml.startTrees
-badtrees_unique=$root/snakes.raxml.startTrees.unique
+badtrees=$root/snakes.raxml.startTree
+badtrees_unique=$root/snakes.raxml.startTree.unique
 outdir=$root
 
 python3 ${build_start_trees_script} --msa_path ${ali_for_raxml} --partitions_path $model --seed 42 --name snakes --num_parsimony_trees 10 --num_random_trees 10
 
 python3 ${netrax_script} --name "snakes_single_best" --msa_path $ali --partitions_path $model --likelihood_type best --brlen_linkage_type linked --seed 42 --start_networks $besttree --good_start
 python3 ${netrax_script} --name "snakes_single_average" --msa_path $ali --partitions_path $model --likelihood_type average --brlen_linkage_type linked --seed 42 --start_networks $besttree --good_start
-python3 ${netrax_script} --name "snakes_multi_best" --msa_path $ali --partitions_path $model --likelihood_type best --brlen_linkage_type linked --seed 42 --start_networks $mltrees_unique --good_start
-python3 ${netrax_script} --name "snakes_multi_average" --msa_path $ali --partitions_path $model --likelihood_type average --brlen_linkage_type linked --seed 42 --start_networks $mltrees_unique --good_start
+python3 ${netrax_script} --name "snakes_multi_best" --msa_path $ali --partitions_path $model --likelihood_type best --brlen_linkage_type linked --seed 42 --start_networks $badtrees_unique
+python3 ${netrax_script} --name "snakes_multi_average" --msa_path $ali --partitions_path $model --likelihood_type average --brlen_linkage_type linked --seed 42 --start_networks $badtrees_unique
