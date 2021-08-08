ef8a266218003d8d05aa8843b6df56b7f66978db
Sarah Lutteropp <luttersh@hitssv543.villa-bosch.de>
2021-08-08 11:06:42 +0200

    fixed typo

--- experiments/assemble_snakes/run_netrax_on_snakes.sh
+++ experiments/assemble_snakes/run_netrax_on_snakes.sh
@@ -9,6 +9,6 @@ model=$root/snakes_partitions.txt
 raxtree=$root/assemble_snakes.raxml.bestTree
 outdir=$root
 
-time $raxml --msa $ali_for_raxml --model $model -seed 42 --prefix $outdir/assemle_snakes --redo | tee raxml_snakes_output.txt
+time $raxml --msa $ali_for_raxml --model $model -seed 42 --prefix $outdir/assemble_snakes --redo | tee raxml_snakes_output.txt
 time mpiexec $netrax --msa $ali --model $model --seed 42 --output $outdir/snakes_best_inferred_network.nw --best_displayed_tree_variant --brlen linked --start_network $raxtree --good_start | tee netrax_snakes_best_output.txt
 time mpiexec $netrax --msa $ali --model $model --seed 42 --output $outdir/snakes_average_inferred_network.nw --average_displayed_tree_variant --brlen linked --start_network $raxtree --good_start | tee netrax_snakes_average_output.txt
