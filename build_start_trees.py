963a036cde8616f1690126852f6e1ec6dc18694f
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2021-08-08 17:52:03 +0200

    updated build start trees script

--- build_start_trees.py
+++ build_start_trees.py
@@ -32,15 +32,23 @@ def find_unique_trees(trees_file):
     p = subprocess.run(raxml_cmd.split(), stdout=subprocess.PIPE, check=True)
 
 
-def build_trees(msa_path, partitions_path, seed, start_trees_output_path, no_inference, keep_only_unique, num_parsimony_trees, num_random_trees):
+def build_trees(msa_path, partitions_path, seed, start_trees_output_path, no_inference, take_only_best_tree, keep_only_unique, num_parsimony_trees, num_random_trees):
     run_raxml(msa_path, partitions_path, seed, start_trees_output_path, no_inference, num_parsimony_trees, num_random_trees)
     trees = []
-    with open(start_trees_output_path + ".raxml.startTrees") as f:
+    trees_path = ""
+    if no_inference:
+        trees_path = start_trees_output_path + ".raxml.startTrees"
+    elif take_only_best_tree:
+        trees_path = start_trees_output_path + ".raxml.bestTree"
+    else:
+        trees_path = start_trees_output_path + ".raxml.mlTrees"
+
+    with open(trees_path) as f:
         trees = f.readlines()
         f.close()
 
-    if keep_only_unique:
-        trees = find_unique_trees(start_trees_output_path + ".raxml.startTrees")
+    if (keep_only_unique) and (take_only_best_tree == False):
+        trees = find_unique_trees(trees_path)
     with open(start_trees_output_path, 'w') as g:
         for tree in trees:
             if len(tree.strip()) > 0:
@@ -56,13 +64,14 @@ def parse_command_line_arguments_build_start_trees():
     CLI.add_argument("--seed", type=int, default=0)
     CLI.add_argument("--start_trees_output_path", type=str)
     CLI.add_argument("--no_inference", action='store_true')
+    CLI.add_argument("--take_only_best_tree", action='store_true')
     CLI.add_argument("--keep_only_unique", action='store_true')
     CLI.add_argument("--num_parsimony_trees", type=int, default=10)
     CLI.add_argument("--num_random_trees", type=int, default=10)
     args = CLI.parse_args()
-    return args.msa_path, args.partitions_path, args.seed, args.start_trees_output_path, args.no_inference, args.keep_only_unique, args.num_parsimony_trees, args.num_random_trees
+    return args.msa_path, args.partitions_path, args.seed, args.start_trees_output_path, args.no_inference, args.take_only_best_tree, args.keep_only_unique, args.num_parsimony_trees, args.num_random_trees
 
 
 if __name__ == '__main__':
-    msa_path, partitions_path, seed, start_trees_output_path, no_inference, keep_only_unique, num_parsimony_trees, num_random_trees = parse_command_line_arguments_build_start_trees()
-    build_trees(msa_path, partitions_path, seed, start_trees_output_path, no_inference, keep_only_unique, num_parsimony_trees, num_random_trees)
+    msa_path, partitions_path, seed, start_trees_output_path, no_inference, take_only_best_tree, keep_only_unique, num_parsimony_trees, num_random_trees = parse_command_line_arguments_build_start_trees()
+    build_trees(msa_path, partitions_path, seed, start_trees_output_path, no_inference, take_only_best_tree, keep_only_unique, num_parsimony_trees, num_random_trees)
