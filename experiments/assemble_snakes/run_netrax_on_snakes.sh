f122bfcd42bab2e2f96a80e011bd78ee1f5557e8
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2021-08-08 10:28:47 +0200

    added snakes binary data to the repo

--- experiments/assemble_snakes/run_netrax_on_snakes.sh
+++ experiments/assemble_snakes/run_netrax_on_snakes.sh
@@ -3,11 +3,12 @@
 root=/home/sarah/eclipse-workspace/NetRAX/experiments/assemble_snakes
 netrax=/home/sarah/eclipse-workspace/NetRAX/bin/netrax
 raxml=/home/sarah/eclipse-workspace/NetRAX/experiments/deps/raxml-ng
-ali=$root/snakes_msa.fasta.raxml.rba
+ali_for_raxml=$root/snakes_msa.fasta
+ali=$root/assemble_snakes.raxml.rba
 model=$root/snakes_partitions.txt
-raxtree=$root/snakes.raxml.bestTree
+raxtree=$root/assemble_snakes.raxml.bestTree
 outdir=$root
 
-time $raxml --msa $ali --model $model -seed 42 --prefix $outdir --redo
+#time $raxml --msa $ali_for_raxml --model $model -seed 42 --prefix $outdir --redo
 time mpiexec $netrax --msa $ali --model $model --seed 42 --output $outdir/snakes_best_inferred_network.nw --best_displayed_tree_variant --brlen linked --start_network $raxtree --good_start
 time mpiexec $netrax --msa $ali --model $model --seed 42 --output $outdir/snakes_average_inferred_network.nw --average_displayed_tree_variant --brlen linked --start_network $raxtree --good_start
