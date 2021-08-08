56966eb80df4c345be5fd2b5cd0d9a491f54ad02
Sarah Lutteropp <sarah.lutteropp@gmail.com>
2021-08-08 18:26:05 +0200

    build start trees works now, in multiple versions

--- build_start_trees.py
+++ build_start_trees.py
@@ -26,11 +26,30 @@ def run_raxml(msa_path, partitions_path, seed, start_trees_output_path, no_infer
     p = subprocess.run(raxml_cmd.split(), stdout=subprocess.PIPE, check=True)
 
 
-def find_unique_trees(trees_file):
-    raxml_cmd = RAXML_PATH + " --rfdist --tree " + trees_file + " --prefix RF --redo"
+def find_unique_trees(trees_file, trees, start_trees_output_path):
+    rf_dist_file = start_trees_output_path + ".raxml.rfDistances"
+    raxml_cmd = RAXML_PATH + " --rfdist --tree " + trees_file + " --prefix " + start_trees_output_path + " --redo"
     print(raxml_cmd)
     p = subprocess.run(raxml_cmd.split(), stdout=subprocess.PIPE, check=True)
-
+    lines = open(rf_dist_file).readlines()
+    os.remove(rf_dist_file)
+    
+    pairwise_rf_dist = []
+    bad_trees = set()
+    for line in lines:
+        if len(line.strip()) > 0:
+            splitted = line.strip().split("\t")
+            id1 = int(splitted[0])
+            id2 = int(splitted[1])
+            dist = int(splitted[2])
+            if id1 not in bad_trees and id2 not in bad_trees:
+                if dist == 0:
+                    bad_trees.add(min(id1, id2))
+    good_trees = []
+    for i in range(len(trees)):
+        if i not in bad_trees:
+            good_trees.append(trees[i])
+    return good_trees
 
 def build_trees(msa_path, partitions_path, seed, start_trees_output_path, no_inference, take_only_best_tree, keep_only_unique, num_parsimony_trees, num_random_trees):
     run_raxml(msa_path, partitions_path, seed, start_trees_output_path, no_inference, num_parsimony_trees, num_random_trees)
@@ -49,13 +68,13 @@ def build_trees(msa_path, partitions_path, seed, start_trees_output_path, no_inf
         f.close()
 
     if (keep_only_unique) and (take_only_best_tree == False):
-        trees = find_unique_trees(trees_path)
+        trees = find_unique_trees(trees_path, trees, start_trees_output_path)
     with open(start_trees_output_path, 'w') as g:
         for tree in trees:
             if len(tree.strip()) > 0:
                 g.write(tree.strip() + "\n")
-        g.write(trees)
         g.close()
+    print("\nStart trees for NetRAX written to: " + start_trees_output_path)
 
 
 def parse_command_line_arguments_build_start_trees():
